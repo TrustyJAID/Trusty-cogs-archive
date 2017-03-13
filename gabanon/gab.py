a3e2104edc834690ee87ccb3eaabefbdd742597d
TrustyJAID <TrustyJAID@gmail.com>
2017-03-13 11:33:25 -0600

    Updated gab tag collection file per user

--- gabanon/gab.py
+++ gabanon/gab.py
@@ -15,101 +15,117 @@ import aiohttp
 import asyncio
 import json
 
-class GabBot:
+class Gab:
 
     GABSEARCH = "https://gab.ai/search/{}"
 
     def __init__(self, bot):
         self.bot = bot
         self.tags = dataIO.load_json("data/gab/gabtags.json")
+        self.servers = ["261565811309674499", "261636805370183691", "288845138887704576"]
 
-    def checktags(self, usertag):
-        return " ".join(value for key, value in self.tags.items() if value in usertag)
-
-    def checkuser(self, username):
-        return " ".join(key for key, value in self.tags.items() if key in username)
-
-    def gettags(self, usertag):
-        return " ".join(value for key, value in self.tags.items() if key in usertag)
-
-    def getuser(self, username):
-        return " ".join(key for key, value in self.tags.items() if value in username)
-
-    def savetags(self, usertag, username):
-        self.tags[username] = usertag
+    def savetags(self, server, usertag, username):
+        self.tags[server][username] = usertag
         dataIO.save_json("data/gab/gabtags.json", self.tags)
         return
 
     @commands.command(hidden=False)
     async def patreon(self):
         """Help on petreon!"""
-        await self.bot.say("Support <@142525247357321216>  and their server/work here: https://www.patreon.com/gabanon")
+        await self.bot.say("Support <@142525247357321216>  and their\
+                            server/work here: https://www.patreon.com/gabanon")
 
     @commands.command(hiddent=False, pass_context=True)
     async def invite(self, ctx):
         """Invite link to gabanon"""
-        if ctx.message.server.id == "261565811309674499" or ctx.message.channel.id == "268584315401666561":
+        if ctx.message.server.id in self.servers:
             await self.bot.say("https://discord.gg/WswbEcJ")
+
+    @commands.command(pass_context=True, name="gabanon", aliases=["g", "gaba"])
+    async def gabanon(self, ctx):
+        """GabAnon"""
+        if ctx.message.server.id in self.servers:
+            chn = ctx.message.channel
+            fn = "data/gab/gabanon.png"
+            await self.bot.send_file(chn, fn)
+
+    @commands.command(pass_context=True)
+    async def freedomfriday(self, ctx):
+        """FreedomFriday"""
+        if ctx.message.server.id in self.servers:
+            chn = ctx.message.channel
+            fn = "data/gab/freedomfriday.jpg"
+            await self.bot.send_file(chn, fn)
 
     @commands.command(pass_context=True)
     @checks.admin_or_permissions(manage_roles=True)
-    async def changegab(self, ctx, usertag, username=""):
+    async def changegab(self, ctx, usertag, username: discord.Member=None):
         """Lets a user change their gab tag"""
-        if ctx.message.channel.id == "266384992718946305" or ctx.message.channel.id == "268584315401666561":
-            if username == "":
-                username = str(ctx.message.author)
-            checktags = self.checktags(usertag)
-            checkuser = self.checkuser(username)
-            if checkuser != username:
-                await self.bot.say("You have not supplied a gab tag before, please type `;gab gabtag` to be added to the list!")
-            if checktags != usertag and username == checkuser:
-                self.savetags(usertag, username)
-                if username == "":
-                    await self.bot.say("{0} Your gab tag has been updated to {1}!".format(ctx.message.author.mention, usertag))
-                else:
-                    await self.bot.say("@{0} Your gab tag has been updated to {1}!".format(username, usertag))
+        server = ctx.message.server.id
+        if server not in self.servers:
+            await self.bot.say("This only works on the GabAnon Server!")
+            return
 
-        else:
-            await self.bot.say("This only works in the #gab-tags channel!")
+            if username is None:
+                username = str(ctx.message.author)
+            if username in self.tags[server]:
+                await self.bot.say("You have not supplied a gab tag before, please \
+                                   type `;gab gabtag` to be added to the list!")
+            if usertag not in self.tags[server]:
+                self.savetags(server, usertag, username)
+                if username == "":
+                    msg = "{0} Your gab tag has been updated to {1}!"
+                    await self.bot.say(msg.format(ctx.message.author.mention,
+                                                  usertag))
+                else:
+                    msg = "@{0} Your gab tag has been updated to {1}!"
+                    await self.bot.say(msg.format(username, usertag))
 
     @commands.command(pass_context=True)
     @checks.admin_or_permissions(manage_roles=True)
     async def remuser(self, ctx, *, username: discord.Member=None):
         """Remomves gab tag and user"""
-        if ctx.message.server.id == "261565811309674499" or ctx.message.channel.id == "268584315401666561":
-            checkuser = self.checkuser(str(username))
-            if checkuser != "":
-                del self.tags[checkuser]
-                await self.bot.say("{} has been removed from the list!".format(username))
+        server = ctx.message.server.id
+        if server in self.servers:
+            if str(username.id) in self.tags[server]:
+                del self.tags[server][str(username.id)]
+                await self.bot.say("{} has been removed from the list!"
+                                   .format(username))
                 dataIO.save_json("data/gab/gabtags.json", self.tags)
             else:
-                await self.bot.say("That username is not in the list or has already been removed!")
+                msg = "That username is not in the list or has already been removed!"
+                await self.bot.say(msg)
 
     @commands.command(pass_context=True)
     @checks.admin_or_permissions(manage_roles=True)
     async def remgab(self, ctx, gabtag):
         """Remomves gab tag and user"""
-        if ctx.message.server.id == "261565811309674499" or ctx.message.channel.id == "268584315401666561":
-            checktag = self.checktags(gabtag)
-            getuser = self.getuser(gabtag)
-            if checktag != "":
-                del self.tags[getuser]
+        server = ctx.message.server.id
+        tags = self.tags[server]
+        if ctx.message.server.id in self.servers:
+            if gabtag in tags.values():
+                for key, value in tags.items():
+                    if gabtag in value:
+                        del tags[key]
+                        break
                 await self.bot.say("{} has been removed from the list!".format(gabtag))
                 dataIO.save_json("data/gab/gabtags.json", self.tags)
             else:
-                await self.bot.say("That gab tag is not in the list or has already been removed!")
+                msg = "That gab tag is not in the list or has already been removed!"
+                await self.bot.say(msg)
 
     @commands.command(pass_context=True)
     @checks.admin_or_permissions(manage_roles=True)
     async def listgab(self, ctx):
         """Lists all received gab tags"""
-        if ctx.message.server.id == "261565811309674499" or ctx.message.channel.id == "268584315401666561":
+        server = ctx.message.server.id
+        if server in self.servers:
             message = "```"
             username = ""
             gabtag = ""
             arrow = ""
             count = 0
-            for key, value in self.tags.items():
+            for key, value in self.tags[server].items():
                 try:
                     username += ctx.message.server.get_member(key).name + "\n"
                 except AttributeError:
@@ -152,35 +168,58 @@ class GabBot:
     @commands.command(pass_context=True, aliases=["Gab", "GAB"])
     async def gab(self, ctx, usertag):
         """Add your gab tag to receive the role Anonymous"""
-        if ctx.message.channel.id == "266384992718946305" or ctx.message.channel.id == "268584315401666561":
-            if "<@" in usertag:
-                usertag = ctx.message.author.name
-            username = ctx.message.author.id
-            checktags = self.checktags(usertag)
-            checkuser = self.checkuser(username)
-            if checktags == "" and checkuser != "":
-                await self.bot.say("You have already supplied a gab tag {}!".format(ctx.message.author.mention))
-            if checktags == usertag and checkuser == "":
-                await self.bot.say("{} That Gab tag is already in use!".format(ctx.message.author.mention))
-            if checktags != usertag and checkuser == "":
-                self.savetags(usertag, username)
-                if ctx.message.channel.id != "268584315401666561":
-                    await self.addgabrole(ctx, "Anonymous")
-                    await self.addgabrole(ctx, "Guest")
-                await self.bot.say("Hello {}, welcome to #GabAnon!".format(ctx.message.author.mention))
-                asyncio.sleep(2)
-                if ctx.message.channel.id != "268584315401666561":
-                    await self.bot.remove_roles(ctx.message.author, self.getroles(ctx, "newcomer"))
-            if checktags != "" and checkuser != "":
-                await self.bot.say("You have already supplied the same gab tag {}!"
-                                   .format(ctx.message.author.mention))
-        else:
+        server = ctx.message.server.id
+        servername = ctx.message.server.name
+        if server not in self.servers:
             await self.bot.say("Please supply the gab tag in <#266384992718946305>!")
+            return
+
+        try:
+            self.tags[server]
+        except KeyError:
+            self.tags[server] = {}
+
+        if "<@" in usertag:
+            usertag = ctx.message.author.name
+        username = ctx.message.author.id
+        if username in self.tags[server]:
+            await self.bot.say("You have already supplied a gab tag {}!"
+                               .format(ctx.message.author.mention))
+            return
+
+        if usertag in self.tags[server].values():
+            await self.bot.say("{} That Gab tag is already in use!"
+                               .format(ctx.message.author.mention))
+            return
+        else:
+            self.savetags(server, usertag, username)
+            if server == "261565811309674499":
+                await self.addgabrole(ctx, "Anonymous")
+                await self.addgabrole(ctx, "Guest")
+            await self.bot.say("Hello {0}, welcome to {1}!"
+                               .format(ctx.message.author.mention, servername))
+            asyncio.sleep(2)
+            if server == "261565811309674499":
+                await self.bot.remove_roles(ctx.message.author, self.getroles(ctx, "newcomer"))
+
         # TODO: Remove gab tag after message sent
+
+def check_folder():
+    if not os.path.exists("data/gab"):
+        print("Creating data/gab folder")
+        os.makedirs("data/gab")
+
+
+def check_file():
+    data = {"": {"": ""}}
+    f = "data/gab/gabtags.json"
+    if not dataIO.is_valid_json(f):
+        print("Creating default gabtags.json...")
+        dataIO.save_json(f, data)
 
 
 def setup(bot):
-    n = GabBot(bot)
-    # bot.add_listener(n.gab, "on_message")
-    bot.add_cog(n)
+    check_folder()
+    check_file()
+    bot.add_cog(Gab(bot))
 
