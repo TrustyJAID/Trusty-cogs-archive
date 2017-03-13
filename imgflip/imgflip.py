f1981679011cc72ba057c22d110f74ced8045275
TrustyJAID <TrustyJAID@gmail.com>
2017-03-13 11:33:25 -0600

    Updated gab tag collection file per user
    Former-commit-id: 722dd6cb6ee2cbb95752ded01072d6415546cdce [formerly a3e2104edc834690ee87ccb3eaabefbdd742597d]
    Former-commit-id: d9cf68166de5f464a0dca74a4b17b00eadd1f760

--- imgflip/imgflip.py
+++ imgflip/imgflip.py
@@ -1,6 +1,10 @@
 from discord.ext import commands
+from discord.ext import commands
+from .utils.chat_formatting import box
+from .utils.dataIO import dataIO
+from .utils import checks
 import aiohttp
-import dataIO  # IO settings, proverbs, etc
+import os
 
 class Imgflip:
 
@@ -8,11 +12,13 @@ class Imgflip:
         self.bot = bot
         self.settings_file = "data/imgflip/settings.json"
         self.settings = dataIO.load_json(self.settings_file)
+        self.search = "https://api.imgflip.com/caption_image?template_id={0}&username={1}&password={2}&text0={3}&text1={4}"
 
-    @commands.command()
-    async def memes(self, *, memeText:str):
+    @commands.command(pass_context=True)
+    async def meme(self, ctx, *, memeText:str):
         """ Pulls a custom meme from imgflip"""
         msg = memeText.split(";")
+        prefix = self.get_prefix(ctx.message.server, ctx.message.content)
         if len(msg) == 3:
             if len(msg[0]) > 1 and len([msg[1]]) < 20 and len([msg[2]]) < 20:
                 try:
@@ -21,7 +27,7 @@ class Imgflip:
                     memeid = msg[0]
                     text1 = msg[1]
                     text2 = msg[2]
-                    search = "https://api.imgflip.com/caption_image?template_id={0}&username={1}&password={2}&text0={3}&text1={4}".format(memeid, username, password, text1, text2)
+                    search = self.search.format(memeid, username, password, text1, text2)
                     async with aiohttp.get(search) as r:
                         result = await r.json()
                     if result["data"] != []:
@@ -31,9 +37,16 @@ class Imgflip:
                     error = result["error_message"]
                     await self.bot.say(error)
             else:
-                await self.bot.say("`" + settings["PREFIX"] + "meme id;text1;text2   |  " + settings["PREFIX"] + "meme help for full list`")
+                await self.bot.say("`" + prefix + "meme id;text1;text2`")
         else:
-            await self.bot.say("`" + settings["PREFIX"] + "meme id;text1;text2   |  " + settings["PREFIX"] + "meme help for full list`")
+            await self.bot.say("`" + prefix + "meme id;text1;text2`")
+
+    def get_prefix(self, server, msg):
+        prefixes = self.bot.settings.get_prefixes(server)
+        for p in prefixes:
+            if msg.startswith(p):
+                return p
+        return None
 
     @commands.group(pass_context=True, name='imgflipset')
     @checks.admin_or_permissions(manage_server=True)
@@ -44,7 +57,7 @@ class Imgflip:
 
     @_imgflipset.command(pass_context=True, name='username')
     @checks.is_owner()
-    async def set_consumer_key(self, ctx, cons_key):
+    async def set_username(self, ctx, cons_key):
         """Sets the consumer key"""
         message = ""
         if cons_key is not None:
@@ -58,7 +71,7 @@ class Imgflip:
 
     @_imgflipset.command(pass_context=True, name='password')
     @checks.is_owner()
-    async def set_consumer_secret(self, ctx, cons_secret):
+    async def set_password(self, ctx, cons_secret):
         """Sets the consumer secret"""
         message = ""
         if cons_secret is not None:
@@ -86,4 +99,6 @@ def check_file():
 
 
 def setup(bot):
+    check_folder()
+    check_file()
     bot.add_cog(Imgflip(bot))
