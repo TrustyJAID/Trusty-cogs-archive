dabfd6a1dda2b5ec7575465ef1bb9a085092464d
TrustyJAID <TrustyJAID@gmail.com>
2017-03-19 03:31:34 -0600

    Replaced urllib with aiohttp
    Replaced existing urllib calls with aiohttp and added a unicode stripper
    for blockchain text data
    
    
    Former-commit-id: 87cbcafe0c84b9a59c13bda6ef9dc78321047c59 [formerly 60975abfc4998fc5056e1492b696897066b71099]
    Former-commit-id: 470e3c1acc88752df9e5cb960a8c2f37022683d0

--- weather/weather.py
+++ weather/weather.py
@@ -5,7 +5,6 @@ from random import randint
 from random import choice
 from enum import Enum
 from binascii import hexlify
-import urllib.request
 import json
 import hashlib
 import datetime
@@ -18,44 +17,54 @@ class weather:
 
     def __init__(self, bot):
         self.bot = bot
+        self.unit = {
+            "imperial": {"code": ["i", "f"], "speed": "mph", "temp": "°F"},
+            "metric": {"code": ["m", "c"], "speed": "km/s", "temp": "°C"},
+            "kelvin": {"code": ["k", "s"], "speed": "km/s", "temp": "°K"}}
 
     @commands.command(pass_context=True, name="weather", aliases=["we"])
     async def weather(self, ctx, city, country="", units="imperial"):
-        if country == "metric" or country == "imperial" or country == "kelvin":
-            units = country
-        if country == "m" or country == "i" or country == "k":
-            unitconvert = {"i":"imperial", "m":"metric", "k": "kelvin"}
-            units = unitconvert[country]
-        if units == "m" or units == "i" or units == "k":
-            unitconvert = {"i":"imperial", "m":"metric", "k": "kelvin"}
-            units = unitconvert[units]
+        await self.bot.send_typing(ctx.message.channel)
+        await self.getweather(ctx, city, country, units)
+
+    async def getweather(self, ctx, city, country="", units="imperial"):
+        if country != "":
+            for key, value in self.unit.items():
+                if country.lower() in value["code"] or country.lower() == key:
+                    units = key
         if units == "kelvin":
-            data = "http://api.openweathermap.org/data/2.5/weather?q={0}&appid=88660f6af079866a3ef50f491082c386&units=metric".format(city+country)
+            url = "http://api.openweathermap.org/data/2.5/weather?q={0}&appid=88660f6af079866a3ef50f491082c386&units=metric".format(city+country)
         else:
-            data = "http://api.openweathermap.org/data/2.5/weather?q={0}&appid=88660f6af079866a3ef50f491082c386&units={1}".format(city+country, units)
-        data = urllib.request.urlopen(data)
-        data = json.loads(data.read())
-        speed = {"metric":"km/h", "imperial":"mph", "kelvin": "km/h"}
-        temp = {"metric":"°C", "imperial":"°F", "kelvin": "°K"}
+            url = "http://api.openweathermap.org/data/2.5/weather?q={0}&appid=88660f6af079866a3ef50f491082c386&units={1}".format(city+country, units)
+        with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                data = await resp.json()
         currenttemp = data["main"]["temp"]
         mintemp = data["main"]["temp_min"]
         maxtemp = data["main"]["temp_max"]
+        city = data["name"]
+        country = data["sys"]["country"]
+        lat, lon = data["coord"]["lat"], data["coord"]["lon"]
+        condition = ', '.join(info["main"] for info in data["weather"])
+        windspeed = str(data["wind"]["speed"]) + " " + self.unit[units]["speed"]
         if units == "kelvin":
-            currenttemp = currenttemp - 273.15
-            mintemp = maxtemp - 273.15
-            maxtemp = maxtemp - 273.15
+            currenttemp = abs(currenttemp - 273.15)
+            mintemp = abs(maxtemp - 273.15)
+            maxtemp = abs(maxtemp - 273.15)
         sunrise = datetime.datetime.fromtimestamp(data["sys"]["sunrise"]).strftime("%H:%M")
         sunset = datetime.datetime.fromtimestamp(data["sys"]["sunset"]).strftime("%H:%M")
         embed = discord.Embed(colour=discord.Colour.blue())
-        embed.add_field(name="🌍 **Location**", value=data["name"] + ", " + data["sys"]["country"], inline=True)
-        embed.add_field(name="📏 **Lat,Long**", value=str(data["coord"]["lat"]) + ", " + str(data["coord"]["lon"]), inline=True)
-        embed.add_field(name="☁ **Condition**", value=', '.join(info["main"] for info in data["weather"]), inline=True)
-        embed.add_field(name="😓 **Humidity**", value=data["main"]["humidity"], inline=True)
-        embed.add_field(name="💨 **Wind Speed**", value=str(data["wind"]["speed"]) + speed[units], inline=True)
-        embed.add_field(name="🌡 **Temperature**", value=str(currenttemp) + temp[units], inline=True)
-        embed.add_field(name="🔆 **Min - Max**", value=str(mintemp) + temp[units]+ " - " + str(maxtemp) + temp[units], inline=True)
-        embed.add_field(name="🌄 **Sunrise (utc)**", value=sunrise, inline=True)
-        embed.add_field(name="🌇 **Sunset (utc)**", value=sunset, inline=True)
+        embed.add_field(name="🌍 **Location**", value="{0}, {1}".format(city, country))
+        embed.add_field(name="📏 **Lat,Long**", value="{0}, {1}".format(lat, lon))
+        embed.add_field(name="☁ **Condition**", value=condition)
+        embed.add_field(name="😓 **Humidity**", value=data["main"]["humidity"])
+        embed.add_field(name="💨 **Wind Speed**", value="{0}".format(windspeed))
+        embed.add_field(name="🌡 **Temperature**", value="{0:.2f}{1}"
+                        .format(currenttemp, self.unit[units]["temp"]))
+        embed.add_field(name="🔆 **Min - Max**", value="{0:.2f}{1} to {2:.2f}{3}"
+                        .format(mintemp, self.unit[units]["temp"], maxtemp, self.unit[units]["temp"]))
+        embed.add_field(name="🌄 **Sunrise (utc)**", value=sunrise)
+        embed.add_field(name="🌇 **Sunset (utc)**", value=sunset)
         embed.set_footer(text="Powered by http://openweathermap.org")
         await self.bot.send_message(ctx.message.channel, embed=embed)
 
