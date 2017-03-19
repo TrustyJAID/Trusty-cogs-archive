ed61e7e376d333bc0fdad0fb956fccdc13285545
TrustyJAID <TrustyJAID@gmail.com>
2017-03-19 03:31:34 -0600

    Replaced urllib with aiohttp
    Replaced existing urllib calls with aiohttp and added a unicode stripper
    for blockchain text data
    
    
    Former-commit-id: c33b14630dd31e4e6d9654b9be2ebad184da2212 [formerly e34b1abecf4aab9728e9a45805f01889581d3016]
    Former-commit-id: a121a1f128975a820f0af5a30e78127e11c60784

--- conversions/conversions.py
+++ conversions/conversions.py
@@ -7,7 +7,7 @@ from random import randint
 from random import choice
 from enum import Enum
 from binascii import hexlify
-import urllib.request
+import requests
 import hashlib
 import datetime
 import time
@@ -27,40 +27,65 @@ class Conversions:
     @commands.command(hidden=False)
     async def converttobtc(self, ammount=1.0, currency="USD"):
         """converts to BTC from a given currency."""
+        url = self.btcurl.format(currency.upper(), ammount)
         try:
-            conversion = urllib.request.urlopen(self.btcurl.format(currency.upper(), ammount))
-            for line in conversion:
-                msg = "{0} {1} is {2} BTC ₿".format(ammount, currency.upper(), line.decode('utf8'))
-                embed = discord.Embed(descirption="BTC", colour=discord.Colour.gold())
-                embed.add_field(name="Bitcoin", value=msg)
-                embed.set_thumbnail(url="https://en.bitcoin.it/w/images/en/2/29/BC_Logo_.png")
-                await self.bot.say(embed=embed)
+            with aiohttp.ClientSession() as session:
+                async with session.get(url) as resp:
+                    data = await resp.read()
+            msg = "{0} {1} is {2} BTC ₿".format(ammount, currency.upper(), float(data))
+            embed = discord.Embed(descirption="BTC", colour=discord.Colour.gold())
+            embed.add_field(name="Bitcoin", value=msg)
+            embed.set_thumbnail(url="https://en.bitcoin.it/w/images/en/2/29/BC_Logo_.png")
+            await self.bot.say(embed=embed)
         except:
             await self.bot.say("Pick a regular integer and standard currency, please! :smile:")
 
     @commands.command(hidden=False, aliases=["bitcoin", "BTC"])
     async def btc(self, ammount=1.0, currency="USD"):
         """converts from BTC to a given currency."""
+        url = self.btcurl.format(currency.upper(), 1)
         try:
-            conversion = urllib.request.urlopen(self.btcurl.format(currency.upper(), 1))
-            for line in conversion:
-                conversion = (1/float(line.decode('utf8')))*float(ammount)
-                msg = "{0} BTC is {1:.2f} {2}".format(ammount, conversion, currency.upper())
-                embed = discord.Embed(descirption="BTC", colour=discord.Colour.gold())
-                embed.add_field(name="Bitcoin", value=msg)
-                embed.set_thumbnail(url="https://en.bitcoin.it/w/images/en/2/29/BC_Logo_.png")
-                await self.bot.say(embed=embed)
+            with aiohttp.ClientSession() as session:
+                async with session.get(url) as resp:
+                    data = await resp.read()
+            conversion = (1/float(data))*float(ammount)
+            msg = "{0} BTC is {1:.2f} {2}".format(ammount, conversion, currency.upper())
+            embed = discord.Embed(descirption="BTC", colour=discord.Colour.gold())
+            embed.add_field(name="Bitcoin", value=msg)
+            embed.set_thumbnail(url="https://en.bitcoin.it/w/images/en/2/29/BC_Logo_.png")
+            await self.bot.say(embed=embed)
         except:
             await self.bot.say("Pick a regular integer and standard currency, please! :smile:")
+
+    @commands.command(hidden=False, aliases=["ethereum", "ETH"])
+    async def eth(self, ammount=1.0, currency="USD"):
+        """converts from ETH to a given currency."""
+        ETH = "https://etherchain.org/api/statistics/price"
+        try:
+            with aiohttp.ClientSession() as session:
+                async with session.get(ETH) as resp:
+                    data = await resp.json()
+            price = data["data"][-1]["usd"] * ammount
+            if currency.upper() != "USD":
+                price = await self.conversionrate("USD", currency.upper()) * price
+            msg = "{0} ETH is {1:.2f} {2}".format(ammount, price, currency.upper())
+            embed = discord.Embed(descirption="BTC", colour=discord.Colour.dark_grey())
+            embed.add_field(name="Etherium", value=msg)
+            embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/ETHEREUM-YOUTUBE-PROFILE-PIC.png/768px-ETHEREUM-YOUTUBE-PROFILE-PIC.png")
+            await self.bot.say(embed=embed)
+        except:
+            await self.bot.say("Pick a regular integer and standard currency, please! :smile:")
+
 
     @commands.command(hidden=False, aliases=["au", "AU", "Au", "GOLD"])
     async def gold(self, ammount=1, currency="USD"):
         """converts gold in ounces to a given currency."""
         GOLD = "https://www.quandl.com/api/v3/datasets/WGC/GOLD_DAILY_{}.json?api_key=EKvr5W-sJUFVSevcpk4v"
         try:
-            conversion = urllib.request.urlopen(GOLD.format(currency.upper()))
-            conversion = json.loads(conversion.read())
-            price = (conversion["dataset"]["data"][0][1]) * ammount
+            with aiohttp.ClientSession() as session:
+                async with session.get(GOLD.format(currency.upper())) as resp:
+                    data = await resp.json()
+            price = (data["dataset"]["data"][0][1]) * ammount
             msg = "{0} oz of Gold is {1:.2f} {2}".format(ammount, price, currency.upper())
             embed = discord.Embed(descirption="Gold", colour=discord.Colour.gold())
             embed.add_field(name="Gold", value=msg)
@@ -74,11 +99,12 @@ class Conversions:
         """converts silver in ounces to a given currency."""
         SILVER = "https://www.quandl.com/api/v3/datasets/LBMA/SILVER.json?api_key=EKvr5W-sJUFVSevcpk4v"
         try:
-            conversion = urllib.request.urlopen(SILVER)
-            conversion = json.loads(conversion.read())
-            price = (conversion["dataset"]["data"][0][1]) * ammount
+            with aiohttp.ClientSession() as session:
+                async with session.get(SILVER) as resp:
+                    data = await resp.json()
+            price = (data["dataset"]["data"][0][1]) * ammount
             if currency != "USD":
-                price = price * self.conversionrate("USD", currency.upper())
+                price = await self.conversionrate("USD", currency.upper()) * price
             msg = "{0} oz of Silver is {1:.2f} {2}".format(ammount, price, currency.upper())
             embed = discord.Embed(descirption="Silver", colour=discord.Colour.lighter_grey())
             embed.add_field(name="Silver", value=msg)
@@ -92,11 +118,12 @@ class Conversions:
         """converts platinum in ounces to a given currency."""
         PLATINUM = "https://www.quandl.com/api/v3/datasets/JOHNMATT/PLAT.json?api_key=EKvr5W-sJUFVSevcpk4v"
         try:
-            conversion = urllib.request.urlopen(PLATINUM)
-            conversion = json.loads(conversion.read())
-            price = (conversion["dataset"]["data"][0][1]) * ammount
+            with aiohttp.ClientSession() as session:
+                async with session.get(PLATINUM) as resp:
+                    data = await resp.json()
+            price = (data["dataset"]["data"][0][1]) * ammount
             if currency != "USD":
-                price = self.conversionrate("USD", currency.upper()) * price
+                price = await self.conversionrate("USD", currency.upper()) * price
             msg = "{0} oz of Platinum is {1:.2f} {2}".format(ammount, price, currency.upper())
             embed = discord.Embed(descirption="Platinum", colour=discord.Colour.dark_grey())
             embed.add_field(name="Platinum", value=msg)
@@ -110,12 +137,13 @@ class Conversions:
         """Gets current ticker symbol price."""
         stock = "https://www.quandl.com/api/v3/datasets/WIKI/{}.json?api_key=EKvr5W-sJUFVSevcpk4v"
         try:
-            conversion = urllib.request.urlopen(stock.format(ticker.upper()))
-            conversion = json.loads(conversion.read())
+            with aiohttp.ClientSession() as session:
+                async with session.get(stock.format(ticker.upper())) as resp:
+                    data = await resp.json()
             convertrate = 1
             if currency != "USD":
                 convertrate = self.conversionrate("USD", currency.upper())
-            price = (conversion["dataset"]["data"][0][1]) * convertrate
+            price = (data["dataset"]["data"][0][1]) * convertrate
             msg = "{0} is {1:.2f} {2}".format(ticker.upper(), price, currency.upper())
             embed = discord.Embed(descirption="Stock Price", colour=discord.Colour.lighter_grey())
             embed.add_field(name=ticker.upper(), value=msg)
@@ -125,19 +153,21 @@ class Conversions:
 
     @commands.command(hidden=False)
     async def convert(self, ammount=1, currency1="USD", currency2="GBP"):
+        """Converts between different currencies"""
         try:
-            conversion = self.conversionrate(currency1.upper(), currency2.upper()) * ammount
+            conversion = await self.conversionrate(currency1.upper(), currency2.upper()) * ammount
             await self.bot.say("{0} {1} is {2:.2f} {3}".format(ammount, currency1, conversion, currency2))
         except:
             await self.bot.say("Please enter a proper integer and standard currency! :smile:")
 
-    def conversionrate(self, currency1, currency2):
-        """converts platinum in ounces to a given currency."""
+    async def conversionrate(self, currency1, currency2):
+        """Function to convert different currencies"""
         CONVERSIONRATES = "http://api.fixer.io/latest?base={}".format(currency1.upper())
         try:
-            conversion = urllib.request.urlopen(CONVERSIONRATES)
-            conversion = json.loads(conversion.read())
-            conversion = (conversion["rates"][currency2.upper()])
+            with aiohttp.ClientSession() as session:
+                async with session.get(CONVERSIONRATES) as resp:
+                    data = await resp.json()
+            conversion = (data["rates"][currency2.upper()])
             return conversion
         except:
             return "That currency is not valid"
