60975abfc4998fc5056e1492b696897066b71099
TrustyJAID <TrustyJAID@gmail.com>
2017-03-19 03:31:34 -0600

    Replaced urllib with aiohttp
    Replaced existing urllib calls with aiohttp and added a unicode stripper
    for blockchain text data

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -30,7 +30,7 @@ class blockchain:
     def __init__(self, bot):
         self.bot = bot
         self.poll_sessions = []
-        self.list_words = ["ASCII", "Julian", "Assange", "Wikileaks"]
+        self.list_words = ["ASCII", "Julian", "Assange", "Wikileaks", "All"]
 
     async def checksum(self, data):
         """
@@ -70,8 +70,15 @@ class blockchain:
         else:
             await self.bot.say("Nothing significant in transaction {}".format(transaction))
 
-    async def split_long_text(self, data):
-        return [data[i:i+1994] for i in range(0, len(data), 1994)]
+    def split_long_text(self, data):
+        return [data[i:i+1990] for i in range(0, len(data), 1990)]
+
+    def remove_non_ascii(self, data):
+        msg = b""
+        for char in data:
+            if char in range(0, 127):
+                msg += bytes(chr(char).encode("utf8"))
+        return msg
 
     @commands.command(pass_context=True)
     async def txdl(self, ctx, transaction, IO="original"):
@@ -82,6 +89,8 @@ class blockchain:
         satoshi = s
         Send a transaction id like the one below
         ;txdl 4b0cd7e191ef0a14a9b6ab1c5900be534118c20a332ff26407648168d2722a2e o"""
+        chn = ctx.message.channel
+        await self.bot.send_typing(chn)
         for tx in BADTRANSACTION:
             if transaction in tx:
                 await self.bot.say("That transaction is black listed for illegal content.")
@@ -117,43 +126,42 @@ class blockchain:
             significanttx += " ASCII letters found output"
         if search.search_words(indata):
             significanttx += " ASCII letters found input"
-        filesystem.write("data/blockchain/" + IO + "data.txt", dataout, True, "wb")
+        # filesystem.write("data/blockchain/" + IO + "data.txt", dataout, True, "wb")
         if significanttx != '':
             extension = "txt"
             await self.bot.say(significanttx)
-            try:
-                for words in self.list_words:
-                    if words in significanttx and "input" in significanttx:
-                            indata = await self.split_long_text(indata)
-                            for i in indata:
-                                await self.bot.say("```" + i.decode('utf8') + "```")
-                    if words in significanttx and "output" in significanttx:
-                        if "Satoshi" in significanttx:
-                            data = await self.split_long_text(data)
-                            for i in data:
-                                await self.bot.say("```" + i.decode('utf8') + "```")
-                        else:
-                            data = await self.split_long_text(data)
-                            for i in data:
-                                await self.bot.say("```" + i.decode('utf8') + "```")
-            except UnicodeDecodeError:
-                pass
-            if "GIF" in significanttx:
-                extension = "gif"
-            if "7z" in significanttx:
-                extension = "7z"
-            if "GZ" in significanttx:
-                extension = "gz"
-            if "PDF" in significanttx:
-                extension = "pdf"
-            if "PNG" in significanttx:
-                extension = "png"
-            if "JPG" in significanttx:
-                extension = "jpg"
-            await self.bot.send_file(ctx.message.channel, "data/blockchain/" + IO + "data.txt", filename=IO+'data.'+extension)
+            for words in self.list_words:
+                if words in significanttx and "input" in significanttx:
+                        indata = self.split_long_text(self.remove_non_ascii(indata))
+                        for i in indata:
+                            await self.bot.say("```" + i.decode('utf8') + "```")
+                if words in significanttx and "output" in significanttx:
+                    if "Satoshi" in significanttx:
+                        data = self.split_long_text(self.remove_non_ascii(data))
+                        for i in data:
+                            await self.bot.say("```" + i.decode('utf8') + "```")
+                    else:
+                        data = self.split_long_text(self.remove_non_ascii(data))
+                        for i in data:
+                            await self.bot.say("```" + i.decode('utf8') + "```")
+        if "GIF" in significanttx:
+            extension = "gif"
+        if "7z" in significanttx:
+            extension = "7z"
+        if "GZ" in significanttx:
+            extension = "gz"
+        if "PDF" in significanttx:
+            extension = "pdf"
+        if "PNG" in significanttx:
+            extension = "png"
+        if "JPG" in significanttx:
+            extension = "jpg"
+        filename = "data/blockchain/" + IO + "data.{}".format(extension)
+        filesystem.write(filename, dataout, True, "wb")
+        await self.bot.send_file(chn, filename)
         if significanttx == '':
             await self.bot.say("Nothing significant in transaction `{}`".format(transaction))
-            await self.bot.send_file(ctx.message.channel, "data/blockchain/" + IO + "data.txt", filename=IO+'data.txt')
+            await self.bot.send_file(chn, filename)
 
 
 def setup(bot):
