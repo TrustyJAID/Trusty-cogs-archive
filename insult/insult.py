0bff54f95c3c9ffe1cbbca0b531aefedc710f684
TrustyJAID <TrustyJAID@gmail.com>
2017-09-06 12:49:55 -0600

    Modified insult cog to include compliments
    There's no reason to always be so hostile!

---
+++ insult/insult.py
@@ -0,0 +1,83 @@
+import discord
+from discord.ext import commands
+from .utils.dataIO import fileIO
+from random import choice as randchoice
+import os
+
+
+class Insult:
+
+    """Airenkun's Insult Cog"""
+    def __init__(self, bot):
+        self.bot = bot
+        self.insults = fileIO("data/insult/insults.json","load")
+        self.compliments = fileIO("data/insult/compliment.json", "load")
+
+    @commands.command(pass_context=True, no_pm=True, aliases=["takeitback"])
+    async def insult(self, ctx, user : discord.Member=None):
+        """Insult the user"""
+
+        msg = ' '
+        if user != None:
+
+            if ctx.message.author.id == "218773382617890828" and user.id == "245862769054711809":
+                await self.bot.say(user.mention + " you're cute! :smile:")
+
+            if user.id == self.bot.user.id:
+                user = ctx.message.author
+                msg = [" How original. No one else had thought of trying to get the bot to insult itself. I applaud your creativity. Yawn. Perhaps this is why you don't have friends. You don't add anything new to any conversation. You are more of a bot than me, predictable answers, and absolutely dull to have an actual conversation with.", "What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo."]
+                await self.bot.say(user.mention + randchoice(msg))
+
+            else:
+                await self.bot.say(user.mention + msg + randchoice(self.insults))
+        else:
+            await self.bot.say(ctx.message.author.mention + msg + randchoice(self.insults))
+
+    @commands.command(pass_context=True, no_pm=True, aliases=["cpl"])
+    async def compliment(self, ctx, user : discord.Member=None):
+        """Compliment the user"""
+
+        msg = ' '
+        if user != None:
+
+            if ctx.message.author.id == "218773382617890828" and user.id == "245862769054711809":
+                await self.bot.say(user.mention + " you're cute! :smile:")
+                return
+
+            if user.id == self.bot.user.id:
+                user = ctx.message.author
+                msg = [" Hey I appreciate the compliment! :smile:", "No ***YOU'RE*** awesome! :smile:"]
+                await self.bot.say(user.mention + randchoice(msg))
+
+            else:
+                await self.bot.say(user.mention + msg + randchoice(self.compliments))
+        else:
+            await self.bot.say(ctx.message.author.mention + msg + randchoice(self.compliments))
+
+
+def check_folders():
+    folders = ("data", "data/insult/")
+    for folder in folders:
+        if not os.path.exists(folder):
+            print("Creating " + folder + " folder...")
+            os.makedirs(folder)
+
+
+def check_files():
+    """Moves the file from cogs to the data directory. Important -> Also changes the name to insults.json"""
+    insults = {"You ugly as hell damn. Probably why most of your friends are online right?"}
+
+    if not os.path.isfile("data/insult/insults.json"):
+        if os.path.isfile("cogs/put_in_cogs_folder.json"):
+            print("moving default insults.json...")
+            os.rename("cogs/put_in_cogs_folder.json", "data/insult/insults.json")
+        else:
+            print("creating default insults.json...")
+            fileIO("data/insult/insults.json", "save", insults)
+
+
+def setup(bot):
+    check_folders()
+    check_files()
+    n = Insult(bot)
+    bot.add_cog(n)
