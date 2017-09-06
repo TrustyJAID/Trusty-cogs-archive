8d83572a78ce14a184bfa5437e89aaf2604d8571
TrustyJAID <TrustyJAID@gmail.com>
2017-09-06 14:01:24 -0600

    refactored and updated trustybot cog
    Former-commit-id: 042135ebaa2b771a0beaecec855f69adcbeab571 [formerly 6fb98cd2bd66062686f6fdc2577392bb7948bf17]
    Former-commit-id: f56f83375059250821c860f41f666ab68d88d4a2

---
+++ faces/faces.py
@@ -0,0 +1,35 @@
+import discord
+from random import choice
+from discord.ext import commands
+from .utils.dataIO import dataIO
+
+class Faces:
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.faces = dataIO.load_json("data/faces/CIAJapaneseStyleFaces.json")
+    
+    @commands.command(pass_context=True, aliases=["japaneseface"])
+    async def face(self, ctx, number=None):
+        """Japanese Faces at random courtesy of the CIA"""
+        if number is None:
+            await self.bot.say(choice(self.faces))
+            return
+        if "<@" in str(number):
+            random.seed(number.strip("<@!>"))
+            userface = self.faces[random.randint(0, len(self.faces))]
+            await self.bot.say(userface)
+            return
+        if number.isdigit():
+            if int(number) <= len(self.faces):
+                await self.bot.say(self.faces[int(number)-1])
+                return
+            else:
+                await self.bot.say("That number is too large, pick less than {}!"
+                                   .format(len(self.faces)))
+                return
+        if not number.isdigit() and "<@!" not in number:
+            await self.bot.say(self.faces[len(number)])
+
+def setup(bot):
+    bot.add_cog(Faces(bot))
