5914dbdb9de22afe56e2893d0938edb66746e199
TrustyJAID <TrustyJAID@gmail.com>
2017-09-12 11:45:03 -0600

    Adding badges cog
    Former-commit-id: 09462e50dfe9b7b0ce031f59090c2b2bbd967590 [formerly 15e029506c9959f3c556f8c2bed517a494af51f4]
    Former-commit-id: f7c18d47600e62a4df2a894cf99a82602d6e97a1

---
+++ badges/badges.py
@@ -0,0 +1,145 @@
+import discord
+from discord.ext import commands
+from .utils.chat_formatting import *
+from .utils.dataIO import dataIO
+from cogs.utils import checks
+import aiohttp
+import glob
+import os
+
+try:
+    from PIL import Image
+    from PIL import ImageColor
+    from PIL import ImageFont
+    from PIL import ImageDraw
+    from PIL import ImageSequence
+    import numpy as np
+    importavailable = True
+except ImportError:
+    importavailable = False
+
+
+class Badges:
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.files = "data/badges/"
+        self.blank_template = {"cia":"data/badges/cia-template.png", 
+                               "cicada":"data/badges/cicada-template.png", 
+                               "ioi":"data/badges/IOI-template.png",
+                               "fbi":"data/badges/fbi-template.png",
+                               "nsa":"data/badges/nsa-template.png",
+                               "gab":"data/badges/gab-template.png",
+                               "dop":"data/badges/dop-template.png",
+                               "shit":"data/badges/shit-template.png",
+                               "bunker":"data/badges/bunker-template.png"}
+        
+
+    async def dl_image(self, url, ext="png"):
+        with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                test = await resp.read()
+                with open(self.files + "temp." + ext, "wb") as f:
+                    f.write(test)
+    
+    async def get_barcode(self, userid):
+        with aiohttp.ClientSession() as session:
+            async with session.get("http://api-bwipjs.rhcloud.com/?bcid=telepen&text={}&scale=2&includetext".format(userid)) as resp:
+                test = await resp.read()
+                with open(self.files + "bar_code_temp.png", "wb") as f:
+                    f.write(test)
+    
+    async def create_badge(self, user, badge):
+        avatar = user.avatar_url if user.avatar_url != "" else user.default_avatar_url
+        username = user.display_name
+        userid = user.id
+        department = "GENERAL SUPPORT" if user.top_role.name == "@everyone" else user.top_role.name.upper()
+        status = user.status
+        if str(user.status) == "online":
+            status = "ACTIVE"
+        if str(user.status) == "offline":
+            status = "COMPLETING TASK"
+        if str(user.status) == "idle":
+            status = "AWAITING INSTRUCTIONS"
+        if str(user.status) == "dnd":
+            status = "MIA"
+        ext = "png"
+        if "gif" in avatar:
+            ext = "gif"
+        await self.dl_image(avatar, ext)
+        await self.get_barcode(userid)
+        template = Image.open(self.blank_template[badge])
+        template = template.convert("RGBA")
+        avatar = Image.open(self.files + "temp." + ext)
+        barcode = Image.open(self.files + "bar_code_temp.png")
+        barcode = barcode.convert("RGBA")
+        barcode.thumbnail((535,125))
+        template.paste(barcode, (420,520), barcode)
+        # font for user information
+        font1 = ImageFont.truetype("data/badges/arial.ttf", 30)
+        # font for extra information
+        font2 = ImageFont.truetype("data/badges/arial.ttf", 24)
+        draw = ImageDraw.Draw(template)
+        # adds username
+        draw.text((225, 330), str(username), fill=(0, 0, 0), font=font1)
+        # adds ID Class
+        draw.text((225, 400), badge.upper() + "-" + str(user).split("#")[1], fill=(0, 0, 0), font=font1)
+        # adds user id
+        draw.text((250, 115), str(userid), fill=(0, 0, 0), font=font2)
+        # adds user status
+        draw.text((250, 175), status, fill=(0, 0, 0), font=font2)
+        # adds department from top role
+        draw.text((250, 235), department, fill=(0, 0, 0), font=font2)
+        # adds user level
+        draw.text((420, 475), "LEVEL " + str(len(user.roles)), fill="red", font=font1)
+        # adds user level
+        draw.text((60, 585), str(user.joined_at), fill=(0, 0, 0), font=font2)
+        if ext == "gif":
+            for image in glob.glob("data/tempgif/*"):
+                os.remove(image)
+            gif_list = [frame.copy() for frame in ImageSequence.Iterator(avatar)]
+            img_list = []
+            num = 0
+            for frame in gif_list[:18]:
+                watermark = frame.copy()
+                watermark = watermark.convert("RGBA")
+                watermark = watermark.resize((100,100))
+                watermark.putalpha(128)
+                id_image = frame.resize((165, 165))
+                template.paste(watermark, (845,45, 945,145), watermark)
+                template.paste(id_image, (60,95, 225, 260))
+                template.save("data/badges/tempgif/{}.png".format(str(num)))
+                num += 1
+            img_list = [Image.open(file) for file in glob.glob("data/badges/tempgif/*")]
+            template.save("data/badges/tempbadge.gif", save_all=True, append_images=img_list, duration=1, loop=10)
+        else:
+            watermark = avatar.convert("RGBA")
+            watermark.putalpha(128)
+            watermark = watermark.resize((100,100))
+            id_image = avatar.resize((165, 165))
+            template.paste(watermark, (845,45, 945,145), watermark)
+            template.paste(id_image, (60,95, 225, 260))
+            template.save("data/badges/tempbadge.png")
+    
+    @commands.command(hidden=True, pass_context=True)
+    async def badges(self, ctx, badge, user:discord.Member=None):
+        """Creates a badge for [cia, nsa, fbi, dop, ioi]"""
+        if badge.lower() not in self.blank_template:
+            await self.bot.say("That badge doesn't exist yet!")
+            return
+        if user is None:
+            user = ctx.message.author
+        avatar = user.avatar_url if user.avatar_url != "" else user.default_avatar_url
+        ext = "png"
+        if "gif" in avatar:
+            ext = "gif"
+        await self.create_badge(user, badge.lower())
+        await self.bot.send_file(ctx.message.channel, "data/badges/tempbadge." + ext)
+
+
+def setup(bot):
+    if not importavailable:
+        raise NameError("You need to run `pip3 install pillow` and `pip3 install numpy`")
+    n = Badges(bot)
+    bot.add_cog(n)
+
