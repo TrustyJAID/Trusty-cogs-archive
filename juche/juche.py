fbc0c41a0e711731a287f5020740704fb6ed107d
TrustyJAID <TrustyJAID@gmail.com>
2017-10-24 11:38:05 -0600

    Updated to V3 rewrite

--- juche/juche.py
+++ juche/juche.py
@@ -1,18 +1,4 @@
 import discord
-from discord.ext import commands
-from .utils.chat_formatting import *
-from .utils.dataIO import dataIO
-from .utils.dataIO import fileIO
-from cogs.utils import checks
-from random import choice
-from binascii import unhexlify
-import time
-import random
-import hashlib
-import aiohttp
-import asyncio
-import string
-import re
 
 class Juche:
 
@@ -30,14 +16,10 @@ class Juche:
 
     async def on_message(self, message):
         msg = message.content
-        server = message.server
+        guild = message.guild
         channel = message.channel
 
-        if server.id in ["304436539482701825", "321105104931389440"]:
+        if guild.id in [304436539482701825, 321105104931389440]:
             juche = await self.check_date(msg)
             if juche != None:
-                await self.bot.send_message(channel, juche)
-
-def setup(bot):
-    n = Juche(bot)
-    bot.add_cog(n)
+                await channel.send(juche)
