a39ce3a01b3ae14639c4c880f8c031075e837474
TrustyJAID <TrustyJAID@gmail.com>
2017-11-04 19:24:50 -0600

    Updated tarot readings to V3

--- tarot/tarotreading.py
+++ tarot/tarotreading.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from .utils.dataIO import dataIO
+from . import tarot_cards
 import aiohttp
 import json
 import random
@@ -14,19 +14,21 @@ class TarotReading:
 
     def __init__(self, bot):
         self.bot = bot
-        self.tarot = dataIO.load_json("data/tarot/tarot.json")
+        self.tarot_cards = tarot_cards.card_list
+
 
     def get_colour(self):
         colour =''.join([choice('0123456789ABCDEF')for x in range(6)])
         return int(colour, 16)
 
     @commands.group(pass_context=True)
-    async def tarots(self, ctx):
+    async def tarot(self, ctx):
         if ctx.invoked_subcommand is None:
-            await send_cmd_help(ctx)
+            await ctx.send_help()
     
-    @tarots.command(name="life", pass_context=True)
+    @tarot.command(name="life", pass_context=True)
     async def _life(self, ctx, user: discord.Member=None):
+        """Unique reading based on your discord user ID. Doesn't change."""
         card_meaning = ["Past", "Present", "Future", "Potential", "Reason"]
         if user is None:
             user = ctx.message.author
@@ -38,15 +40,19 @@ class TarotReading:
         
         embed = discord.Embed(title="Tarot reading for {}".format(user.display_name),
                               colour=discord.Colour(value=self.get_colour()))
+        embed.set_thumbnail(url=self.tarot_cards[str(cards[-1])]["card_img"])
+        embed.timestamp = ctx.message.created_at
+        embed.set_author(name=user.name, icon_url=user.avatar_url)
         number = 0
         for card in cards:
-            embed.add_field(name="{0}: {1}".format(card_meaning[number], self.tarot[str(card)]["card_name"]),
-                            value=self.tarot[str(card)]["card_meaning"])
+            embed.add_field(name="{0}: {1}".format(card_meaning[number], self.tarot_cards[str(card)]["card_name"]),
+                            value=self.tarot_cards[str(card)]["card_meaning"])
             number += 1
-        await self.bot.send_message(ctx.message.channel, embed=embed)
+        await ctx.send(embed=embed)
     
-    @tarots.command(name="reading", pass_context=True)
+    @tarot.command(name="reading", pass_context=True)
     async def _reading(self, ctx, user: discord.Member=None):
+        """Unique reading as of this very moment."""
         card_meaning = ["Past", "Present", "Future", "Potential", "Reason"]
         if user is None:
             user = ctx.message.author
@@ -56,30 +62,34 @@ class TarotReading:
         
         embed = discord.Embed(title="Tarot reading for {}".format(user.display_name),
                               colour=discord.Colour(value=self.get_colour()))
+        embed.set_thumbnail(url=self.tarot_cards[str(cards[-1])]["card_img"])
+        embed.timestamp = ctx.message.created_at
+        embed.set_author(name=user.name, icon_url=user.avatar_url)
         number = 0
         for card in cards:
-            embed.add_field(name="{0}: {1}".format(card_meaning[number], self.tarot[str(card)]["card_name"]),
-                            value=self.tarot[str(card)]["card_meaning"])
+            embed.add_field(name="{0}: {1}".format(card_meaning[number], self.tarot_cards[str(card)]["card_name"]),
+                            value=self.tarot_cards[str(card)]["card_meaning"])
             number += 1
-        await self.bot.send_message(ctx.message.channel, embed=embed)
+        await ctx.send(embed=embed)
 
 
-    @tarots.command(name="card", pass_context=True)
+    @tarot.command(name="card", pass_context=True)
     async def _card(self, ctx, *, msg=None):
-        user = ctx.message.author.id
+        """Random card or choose a card based on number or name."""
+        user = ctx.message.author
         # msg = message.content
         card = None
 
         if msg is None:
-            card = self.tarot[str(random.randint(1, 78))]
+            card = self.tarot_cards[str(random.randint(1, 78))]
 
         elif msg.isdigit() and int(msg) > 0 and int(msg) < 79:
-            card = self.tarot[str(msg)]
+            card = self.tarot_cards[str(msg)]
         
         elif not msg.isdigit():
-            for cards in self.tarot:
-                if msg.lower() in self.tarot[cards]["card_name"].lower():
-                    card = self.tarot[cards]
+            for cards in self.tarot_cards:
+                if msg.lower() in self.tarot_cards[cards]["card_name"].lower():
+                    card = self.tarot_cards[cards]
             if card is None:
                 await self.bot.say("That card does not exist!")
                 return
@@ -88,9 +98,7 @@ class TarotReading:
                               description=card["card_meaning"],
                               colour=discord.Colour(value=self.get_colour()),
                               url=card["card_url"])
+        embed.timestamp = ctx.message.created_at
+        embed.set_author(name=user.name, icon_url=user.avatar_url)
         embed.set_image(url=card["card_img"])
-        await self.bot.send_message(ctx.message.channel, embed=embed)
-
-
-def setup(bot):
-    bot.add_cog(TarotReading(bot))
+        await ctx.send(embed=embed)
