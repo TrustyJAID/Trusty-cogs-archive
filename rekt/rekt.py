2f5f418c0889638c5fe03b49c4a0720d887eef8e
TrustyJAID <TrustyJAID@gmail.com>
2017-11-05 18:51:47 -0700

    Updated to V3
    Former-commit-id: 5ef725e923bfbf8d4c8c8fbfbf8e4817b2c120d3 [formerly 9fa3af051127bd34cc66a82db4447f82f63d092d]
    Former-commit-id: bb78069cacaecbaf5bcec5857f463f3eb87c67b1

--- rekt/rekt.py
+++ rekt/rekt.py
@@ -1,23 +1,101 @@
 import discord
 from discord.ext import commands
-from .utils.chat_formatting import *
-from .utils.dataIO import dataIO
-from cogs.utils import checks
-import time
-import asyncio
+from redbot.core import checks
+
+rektlist = [
+            "☑ Rekt",
+            "☑ Tyrannosaurus Rekt",
+            "☑ sudo apt-get Rekt",
+            "☑ e=mRekt²",
+            "☑ Rekt and Morty",
+            "☑ Really Rekt",
+            "☑ Cash4Rekt.com",
+            "☑ Grapes of Rekt",
+            "☑ Ship Rekt",
+            "☑ Rektavoir Dogs",
+            "☑ Raiders of the Rekt Ark ",
+            "☑ Indiana Jones and the Temple of Rekt",
+            "☑ Rekt markes the spot",
+            "☑ Caught rekt handed",
+            "☑ The Rekt Side Story",
+            "☑ Singin' In The Rekt",
+            "☑ Painting The Roses Rekt",
+            "☑ Rekt Van Winkle",
+            "☑ Parks and Rekt",
+            "☑ Lord of the Rekts: The Reking of the King",
+            "☑ Star Trekt",
+            "☑ The Rekt Prince of Bel-Air",
+            "☑ A Game of Rekt",
+            "☑ Rektflix",
+            "☑ Rekt it like it's hot",
+            "☑ RektBox 360",
+            "☑ The Rekt-men",
+            "☑ School Of Rekt",
+            "☑ I am Fire, I am Rekt",
+            "☑ Rekt and Roll",
+            "☑ Professor Rekt",
+            "☑ Catcher in the Rekt",
+            "☑ Rekt-22",
+            "☑ Harry Potter: The Half-Rekt Prince",
+            "☑ Great Rektspectations",
+            "☑ Paper Scissors Rekt",
+            "☑ RektCraft",
+            "☑ Grand Rekt Auto V",
+            "☑ Call of Rekt: Modern Reking 2",
+            "☑ Legend Of Zelda: Ocarina of Rekt",
+            "☑ Rekt It Ralph",
+            "☑ Left 4 Rekt",
+            "☑ www.rekkit.com",
+            "☑ Pokemon: Fire Rekt",
+            "☑ The Shawshank Rektemption",
+            "☑ The Rektfather",
+            "☑ The Rekt Knight",
+            "☑ Fiddler on the Rekt",
+            "☑ The Rekt Files",
+            "☑ The Good, the Bad, and The Rekt",
+            "☑ Forrekt Gump",
+            "☑ The Silence of the Rekts",
+            "☑ The Green Rekt",
+            "☑ Gladirekt",
+            "☑ Spirekted Away",
+            "☑ Terminator 2: Rektment Day",
+            "☑ The Rekt Knight Rises",
+            "☑ The Rekt King",
+            "☑ REKT-E",
+            "☑ Citizen Rekt",
+            "☑ Requiem for a Rekt",
+            "☑ REKT TO REKT ass to ass",
+            "☑ Star Wars: Episode VI - Return of the Rekt",
+            "☑ Braverekt",
+            "☑ Batrekt Begins",
+            "☑ 2001: A Rekt Odyssey",
+            "☑ The Wolf of Rekt Street",
+            "☑ Rekt's Labyrinth",
+            "☑ 12 Years a Rekt",
+            "☑ Gravirekt",
+            "☑ Finding Rekt",
+            "☑ The Arekters",
+            "☑ There Will Be Rekt",
+            "☑ Christopher Rektellston",
+            "☑ Hachi: A Rekt Tale",
+            "☑ The Rekt Ultimatum",
+            "☑ Shrekt",
+            "☑ Rektal Exam",
+            "☑ Rektium for a Dream",
+            "☑ www.Trekt.tv",
+            "☑ Erektile Dysfunction"
+            ]
 
 
-class rekt:
+class Rekt:
     def __init__(self, bot):
         self.bot = bot
-        self.STARTTIME = time.time()
-        self.rektlist = dataIO.load_json("data/rekt/rekt.json")
 
     @commands.command(hidden=False, pass_context=True)
-    @commands.cooldown(1, 60, commands.BucketType.server)
+    @commands.cooldown(1, 60, commands.BucketType.guild)
     async def rekt(self, ctx, endcount=10):
         """REKT"""
-        server = str(ctx.message.server.id)
+        guild = str(ctx.message.guild.id)
         user = ctx.message.author.id
         rektemoji = ["\u2611", "\U0001F1F7", "\U0001F1EA", "\U0001F1F0", "\U0001F1F9"]
         embed = discord.Embed(colour=discord.Colour.blue())
@@ -25,7 +103,7 @@ class rekt:
         count = 0
         count2 = 0
         message = ""
-        for line in self.rektlist:
+        for line in rektlist:
             if count2 == 10:
                 embed.add_field(name="REKT", value=message, inline=True)
                 count2 = 0
@@ -39,11 +117,6 @@ class rekt:
         if message != "":
             embed.add_field(name="REKT", value=message, inline=True)
         embed.set_author(name="Are you REKT?")
-        msg = await self.bot.send_message(ctx.message.channel, embed=embed)
+        msg = await ctx.send(embed=embed)
         for emoji in rektemoji:
-            await self.bot.add_reaction(msg, emoji=emoji)
-
-
-def setup(bot):
-    n = rekt(bot)
-    bot.add_cog(n)
+            await msg.add_reaction(emoji=emoji)
