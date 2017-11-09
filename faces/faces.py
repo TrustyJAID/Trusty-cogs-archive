60f4d7a5967b3d8a160d214d7aa64e2b042d3c33
TrustyJAID <TrustyJAID@gmail.com>
2017-11-08 17:59:59 -0700

    Updated faces to V3
    Former-commit-id: e072fee3f8be1b72c040177ff3554736ac0d4c81

--- faces/faces.py
+++ faces/faces.py
@@ -2,35 +2,32 @@ import discord
 from random import choice
 import random
 from discord.ext import commands
-from .utils.dataIO import dataIO
+from . import faceslist
 
 class Faces:
 
     def __init__(self, bot):
         self.bot = bot
-        self.faces = dataIO.load_json("data/faces/CIAJapaneseStyleFaces.json")
+        self.faces = faceslist.faces
     
     @commands.command(pass_context=True, aliases=["japaneseface"])
     async def face(self, ctx, number=None):
         """Japanese Faces at random courtesy of the CIA"""
         if number is None:
-            await self.bot.say(choice(self.faces))
+            await ctx.send(choice(self.faces))
             return
         if "<@" in str(number):
             random.seed(number.strip("<@!>"))
             userface = self.faces[random.randint(0, len(self.faces))]
-            await self.bot.say(userface)
+            await ctx.send(userface)
             return
         if number.isdigit():
             if int(number) <= len(self.faces):
-                await self.bot.say(self.faces[int(number)-1])
+                await ctx.send(self.faces[int(number)-1])
                 return
             else:
-                await self.bot.say("That number is too large, pick less than {}!"
+                await ctx.send("That number is too large, pick less than {}!"
                                    .format(len(self.faces)))
                 return
         if not number.isdigit() and "<@!" not in number:
-            await self.bot.say(self.faces[len(number)])
-
-def setup(bot):
-    bot.add_cog(Faces(bot))
+            await ctx.send(self.faces[len(number)])
