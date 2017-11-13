2a0990cf3ad93ef1d33f4d17fa738195b9647360
TrustyJAID <TrustyJAID@gmail.com>
2017-11-13 11:39:23 -0700

    Updated Halo for V3 and fixed menu error
    Former-commit-id: 5e9f5a8053199b3e91e968fc449f5e6a00b1f0df

--- halo/halo.py
+++ halo/halo.py
@@ -1,10 +1,8 @@
 import discord
 import aiohttp
 from discord.ext import commands
-from .utils.chat_formatting import pagify
-from .utils.dataIO import dataIO
-from .utils import checks
-import os
+from redbot.core import Config
+from redbot.core import checks
 from random import choice as randchoice
 
 numbs = {
@@ -18,11 +16,15 @@ class Halo():
     def __init__(self, bot):
         self.bot = bot
         self.session = aiohttp.ClientSession(loop=self.bot.loop)
-        self.settings = dataIO.load_json("data/halo/settings.json")
-        self.api_token = self.settings["api_token"]
+        default_global = {"api_token":{"token": "",
+                "language": "en"}}
+        self.config = Config.get_conf(self, 35689771456)
+        self.config.register_global(**default_global)
 
     async def request_url(self, url, params=None):
-        async with self.session.get(url, params=params, headers=self.api_token) as resp:
+        header_data = await self.config.api_token()
+        header = {"Ocp-Apim-Subscription-Key": header_data["token"], "Accept-Language":header_data["language"]}
+        async with self.session.get(url, params=params, headers=header) as resp:
             return await resp.json()
 
     @commands.group(pass_context=True, name='halo5')
@@ -30,14 +32,14 @@ class Halo():
     async def _halo5(self, ctx):
         """Get information from Halo 5"""
         if ctx.invoked_subcommand is None:
-            await self.bot.send_cmd_help(ctx)
+            await ctx.send_help()
 
     @commands.group(pass_context=True, name='halowars')
     @checks.admin_or_permissions(manage_server=True)
     async def _halowars(self, ctx):
         """Get information from Halo Wars 2"""
         if ctx.invoked_subcommand is None:
-            await self.bot.send_cmd_help(ctx)
+            await ctx.send_help()
 
     def random_colour(self):
         return int(''.join([randchoice('0123456789ABCDEF')for x in range(6)]), 16)
@@ -48,7 +50,7 @@ class Halo():
         """menu control logic for this taken from
            https://github.com/Lunar-Dust/Dusty-Cogs/blob/master/menu/menu.py"""
         s = post_list[page]
-        created_at = ctx.message.timestamp
+        created_at = ctx.message.created_at
         desc = "Created at: {}".format(created_at)
         em = discord.Embed(title=s["name"],
                            description=s["description"],
@@ -60,42 +62,42 @@ class Halo():
             em.set_image(url=s["imageUrl"])
         if not message:
             message =\
-                await self.bot.send_message(ctx.message.channel, embed=em)
-            await self.bot.add_reaction(message, "⬅")
-            await self.bot.add_reaction(message, "❌")
-            await self.bot.add_reaction(message, "➡")
+                await ctx.send(embed=em)
+            await message.add_reaction("⬅")
+            await message.add_reaction("❌")
+            await message.add_reaction("➡")
         else:
-            message = await self.bot.edit_message(message, embed=em)
-        react = await self.bot.wait_for_reaction(
-            message=message, user=ctx.message.author, timeout=timeout,
-            emoji=["➡", "⬅", "❌"]
-        )
-        if react is None:
-            await self.bot.remove_reaction(message, "⬅", self.bot.user)
-            await self.bot.remove_reaction(message, "❌", self.bot.user)
-            await self.bot.remove_reaction(message, "➡", self.bot.user)
+             # message edits don't return the message object anymore lol
+            await message.edit(embed=em)
+        check = lambda react, user:user == ctx.message.author and react.emoji in ["➡", "⬅", "❌"]
+        try:
+            react, user = await self.bot.wait_for("reaction_add", check=check, timeout=timeout)
+        except asyncio.TimeoutError:
+            await message.remove_reaction("⬅", self.bot.user)
+            await message.remove_reaction("❌", self.bot.user)
+            await message.remove_reaction("➡", self.bot.user)
             return None
-        reacts = {v: k for k, v in numbs.items()}
-        react = reacts[react.reaction.emoji]
-        if react == "next":
-            next_page = 0
-            if page == len(post_list) - 1:
-                next_page = 0  # Loop around to the first item
-            else:
-                next_page = page + 1
-            return await self.halo5_playlist_menu(ctx, post_list, message=message,
-                                         page=next_page, timeout=timeout)
-        elif react == "back":
-            next_page = 0
-            if page == 0:
-                next_page = len(post_list) - 1  # Loop around to the last item
-            else:
-                next_page = page - 1
-            return await self.halo5_playlist_menu(ctx, post_list, message=message,
-                                         page=next_page, timeout=timeout)
         else:
-            return await\
-                self.bot.delete_message(message)
+            reacts = {v: k for k, v in numbs.items()}
+            react = reacts[react.emoji]
+            if react == "next":
+                next_page = 0
+                if page == len(post_list) - 1:
+                    next_page = 0  # Loop around to the first item
+                else:
+                    next_page = page + 1
+                return await self.halo5_playlist_menu(ctx, post_list, message=message,
+                                             page=next_page, timeout=timeout)
+            elif react == "back":
+                next_page = 0
+                if page == 0:
+                    next_page = len(post_list) - 1  # Loop around to the last item
+                else:
+                    next_page = page - 1
+                return await self.halo5_playlist_menu(ctx, post_list, message=message,
+                                             page=next_page, timeout=timeout)
+            else:
+                return await message.delete()
 
     @_halo5.command(pass_context=True, name="playlist")
     async def halo5_playlist(self, ctx, active=True):
@@ -113,7 +115,7 @@ class Halo():
         """menu control logic for this taken from
            https://github.com/Lunar-Dust/Dusty-Cogs/blob/master/menu/menu.py"""
         s = post_list[page]
-        created_at = ctx.message.timestamp
+        created_at = ctx.message.created_at
         desc = "Created at: {}".format(created_at)
         em = discord.Embed(title=s["View"]["Title"],
                            # description=s["description"],
@@ -125,42 +127,42 @@ class Halo():
         em.set_image(url=s["View"]["HW2Playlist"]["Image"]["View"]["Media"]["MediaUrl"])
         if not message:
             message =\
-                await self.bot.send_message(ctx.message.channel, embed=em)
-            await self.bot.add_reaction(message, "⬅")
-            await self.bot.add_reaction(message, "❌")
-            await self.bot.add_reaction(message, "➡")
+                await ctx.send(embed=em)
+            await message.add_reaction("⬅")
+            await message.add_reaction("❌")
+            await message.add_reaction("➡")
         else:
-            message = await self.bot.edit_message(message, embed=em)
-        react = await self.bot.wait_for_reaction(
-            message=message, user=ctx.message.author, timeout=timeout,
-            emoji=["➡", "⬅", "❌"]
-        )
-        if react is None:
-            await self.bot.remove_reaction(message, "⬅", self.bot.user)
-            await self.bot.remove_reaction(message, "❌", self.bot.user)
-            await self.bot.remove_reaction(message, "➡", self.bot.user)
+             # message edits don't return the message object anymore lol
+            await message.edit(embed=em)
+        check = lambda react, user:user == ctx.message.author and react.emoji in ["➡", "⬅", "❌"]
+        try:
+            react, user = await self.bot.wait_for("reaction_add", check=check, timeout=timeout)
+        except asyncio.TimeoutError:
+            await message.remove_reaction("⬅", self.bot.user)
+            await message.remove_reaction("❌", self.bot.user)
+            await message.remove_reaction("➡", self.bot.user)
             return None
-        reacts = {v: k for k, v in numbs.items()}
-        react = reacts[react.reaction.emoji]
-        if react == "next":
-            next_page = 0
-            if page == len(post_list) - 1:
-                next_page = 0  # Loop around to the first item
-            else:
-                next_page = page + 1
-            return await self.halowars_playlist_menu(ctx, post_list, message=message,
-                                         page=next_page, timeout=timeout)
-        elif react == "back":
-            next_page = 0
-            if page == 0:
-                next_page = len(post_list) - 1  # Loop around to the last item
-            else:
-                next_page = page - 1
-            return await self.halowars_playlist_menu(ctx, post_list, message=message,
-                                         page=next_page, timeout=timeout)
         else:
-            return await\
-                self.bot.delete_message(message)
+            reacts = {v: k for k, v in numbs.items()}
+            react = reacts[react.emoji]
+            if react == "next":
+                next_page = 0
+                if page == len(post_list) - 1:
+                    next_page = 0  # Loop around to the first item
+                else:
+                    next_page = page + 1
+                return await self.halowars_playlist_menu(ctx, post_list, message=message,
+                                             page=next_page, timeout=timeout)
+            elif react == "back":
+                next_page = 0
+                if page == 0:
+                    next_page = len(post_list) - 1  # Loop around to the last item
+                else:
+                    next_page = page - 1
+                return await self.halowars_playlist_menu(ctx, post_list, message=message,
+                                             page=next_page, timeout=timeout)
+            else:
+                return await message.delete()
 
     async def get_halo5_rank_data(self, designation_id, tier_id):
         rank_data = await self.request_url("https://www.haloapi.com/metadata/h5/metadata/csr-designations")
@@ -179,11 +181,11 @@ class Halo():
         embed = discord.Embed(title=gamertag,
                               description=designation_name,
                               colour=discord.Colour(value=int(colours[designation_name], 16)),
-                              timestamp=ctx.message.timestamp)
+                              timestamp=ctx.message.created_at)
         embed.add_field(name="Designation", value=str(designation), inline=True)
         embed.add_field(name="Tier", value=str(tier), inline=True)
         embed.set_thumbnail(url=image_url[0])
-        await self.bot.send_message(ctx.message.channel, embed=embed)
+        await ctx.send(embed=embed)
         
 
 
@@ -204,31 +206,11 @@ class Halo():
         """Command for setting required access information for the API.
         To get this info, visit https://developer.haloapi.com and create a new application."""
         if ctx.invoked_subcommand is None:
-            await self.bot.send_cmd_help(ctx)
+            await ctx.send_help()
 
     @_haloset.command(pass_context=True)
     async def tokens(self, ctx, subscription_key, language="en"):
         """Set the tokens and language for requests from the API"""
-        self.settings["api_token"]["Ocp-Apim-Subscription-Key"] = subscription_key
-        self.settings["api_token"]["Accept-Language"] = language
-        dataIO.save_json("data/halo/settings.json", self.settings)
-        await self.bot.send_message(ctx.message.channel, "Halo API credentials set!")
-
-def check_folder():
-    if not os.path.exists("data/halo"):
-        print("Creating data/halo folder")
-        os.makedirs("data/halo")
-
-
-def check_file():
-    data = {"api_token":{"Ocp-Apim-Subscription-Key": "",
-                "Accept-Language": "en"}}
-    f = "data/halo/settings.json"
-    if not dataIO.is_valid_json(f):
-        print("Creating default settings.json...")
-        dataIO.save_json(f, data)
-
-def setup(bot):
-    check_folder()
-    check_file()
-    bot.add_cog(Halo(bot))
+        await self.config.api_token.token.set(subscription_key)
+        await self.config.api_token.language.set(language)
+        await ctx.send("Halo API credentials set!")
