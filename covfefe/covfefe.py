c34602ec500e2b872992b09ccd522d8dde6d596a
TrustyJAID <TrustyJAID@gmail.com>
2017-11-13 13:46:02 -0700

    Updated covfefe to V3 and added to repo
    Former-commit-id: 80281303858b584b6a2401b692df22db96f62e1a [formerly b3931e4acaea1d8dbb99d0ee5ae420cf5236585e]
    Former-commit-id: 25b3f922fbb4c3364cb86d3a1bf6695bf68d748a

---
+++ covfefe/covfefe.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import re
+
+class Covfefe:
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    def covfefe(self, x, k='aeiouy])'):
+        try:
+            b,c,v=re.findall(f'(.*?[{k}([^{k}.*?([{k}',x)[0];return b+c+(('bcdfgkpstvz'+c)['pgtvkgbzdfs'.find(c)]+v)*2
+        except:
+            return None
+    
+    @commands.command()
+    async def covefy(self, ctx, msg):
+        """covfefe"""
+        newword = self.covfefe(msg)
+        if newword is not None:
+            await ctx.send(newword)
+        else:
+            await ctx.send("I cannot covfefeify that word")
