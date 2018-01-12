854971e49055dff1792af40a455663a22b9d8ed2
TrustyJAID <TrustyJAID@gmail.com>
2018-01-11 23:21:38 -0700

    [Badges] Added SpaceX and Q badges

--- badges/badges.py
+++ badges/badges.py
@@ -34,8 +34,11 @@ class Badges:
                                "gab":{"code":"GAB", "loc":"data/badges/gab-template.png"},
                                "dop":{"code":"DOP", "loc":"data/badges/dop-template.png"},
                                "shit":{"code":"SHIT", "loc":"data/badges/shit-template.png"},
+                               "bunker":{"code":"BUNKER", "loc":"data/badges/bunker-template.png"},
                                "nk":{"code":"NK", "loc":"data/badges/nk-template.png"},
                                "kek":{"code":"KEK", "loc": "data/badges/kek-template.png"},
+                               "Q":{"code":"Q !ITPb.qbhqo", "loc":"data/badges/Q-template.png"},
+                               "dhs":{"code":"SpaceX", "loc":"data/badges/spacex-template.png"},
                                "unsc":{"code":"UNSC", "loc": "data/badges/unsc-template.png"}}
 
         
@@ -57,6 +60,22 @@ class Badges:
         for item in datas:
             if item[0] == 255 and item[1] == 255 and item[2] == 255:
                 newData.append((255, 255, 255, 0))
+            else:
+                newData.append(item)
+
+        img.putdata(newData)
+        img.save(self.files + "temp/bar_code_temp.png", "PNG")
+
+    async def invert_barcode(self):
+        """https://stackoverflow.com/questions/765736/using-pil-to-make-all-white-pixels-transparent"""
+        img = Image.open(self.files + "temp/bar_code_temp.png")
+        img = img.convert("RGBA")
+        datas = img.getdata()
+
+        newData = []
+        for item in datas:
+            if item[0] == 0 and item[1] == 0 and item[2] == 0:
+                newData.append((255, 255, 255))
             else:
                 newData.append(item)
 
@@ -85,6 +104,10 @@ class Badges:
                                 writer=ImageWriter(), 
                                 output="data/badges/temp/bar_code_temp")
         await self.remove_white_barcode()
+        fill = (0, 0, 0) # text colour fill
+        if badge == "Q":
+          fill = (255, 255, 255)
+          await self.invert_barcode()
         template = Image.open(self.blank_template[badge]["loc"])
         template = template.convert("RGBA")
         avatar = Image.open(self.files + "temp/temp." + ext)
@@ -98,19 +121,20 @@ class Badges:
         font2 = ImageFont.truetype("data/badges/ARIALUNI.TTF", 24)
         draw = ImageDraw.Draw(template)
         # adds username
-        draw.text((225, 330), str(username), fill=(0, 0, 0), font=font1)
+        
+        draw.text((225, 330), str(username), fill=fill, font=font1)
         # adds ID Class
-        draw.text((225, 400), self.blank_template[badge]["code"] + "-" + str(user).split("#")[1], fill=(0, 0, 0), font=font1)
+        draw.text((225, 400), self.blank_template[badge]["code"] + "-" + str(user).split("#")[1], fill=fill, font=font1)
         # adds user id
-        draw.text((250, 115), str(userid), fill=(0, 0, 0), font=font2)
+        draw.text((250, 115), str(userid), fill=fill, font=font2)
         # adds user status
-        draw.text((250, 175), status, fill=(0, 0, 0), font=font2)
+        draw.text((250, 175), status, fill=fill, font=font2)
         # adds department from top role
-        draw.text((250, 235), department, fill=(0, 0, 0), font=font2)
+        draw.text((250, 235), department, fill=fill, font=font2)
         # adds user level
         draw.text((420, 475), "LEVEL " + str(len(user.roles)), fill="red", font=font1)
         # adds user level
-        draw.text((60, 585), str(user.joined_at), fill=(0, 0, 0), font=font2)
+        draw.text((60, 585), str(user.joined_at), fill=fill, font=font2)
         if ext == "gif":
             for image in glob.glob("data/badges/temp/tempgif/*"):
                 os.remove(image)
@@ -175,9 +199,7 @@ def check_folder():
     if not os.path.exists("data/badges"):
         print("Creating temporary badge folders folder")
         os.makedirs("data/badges")
-    if not os.path.exists("data/badges/temp"):
         os.makedirs("data/badges/temp")
-    if not os.path.exists("data/badges/temp/tempgif"):
         os.makedirs("data/badges/temp/tempgif")
 
 def setup(bot):
