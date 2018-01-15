7bd7f256ca4b3622f6daff712a0f559584641e36
TrustyJAID <TrustyJAID@gmail.com>
2018-01-14 17:08:31 -0700

    [reee] Added ability to set server to post messages
    Former-commit-id: ef0dc7ea4b51328cb82051bf852767d0e073ada6 [formerly e7fd3d54d4038138adf0d4947c12412ab44cdd8d]
    Former-commit-id: 5901cee19fb2976177e1a90add66b21dcba135d4

--- reee/reee.py
+++ reee/reee.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from .utils.chat_formatting import *
 from .utils.dataIO import dataIO
 from cogs.utils import checks
-
+import os
 try:
     from PIL import Image
     importavailable = True
@@ -19,13 +19,14 @@ class Reee:
         self.increment = 2
         self.start_size = (1024, 1024)
         self.smallest = (32, 32)
+        self.settings = dataIO.load_json("data/reee/settings.json")
     
     async def on_message(self, message):
         if len(message.content) < 2 or message.channel.is_private:
             return
-        if message.server.id == "133049272517001216":
+        if message.server.id not in self.settings:
             return
-        if message.author == self.bot.user:
+        if message.author.bot:
             return
         msg = message.content.lower()
         if "reee" in msg:
@@ -41,8 +42,33 @@ class Reee:
         im.thumbnail((length*size, width*size), Image.ANTIALIAS)
         im.save("data/reee/newreee.png")
 
+    @commands.command(pass_context=True)
+    @checks.mod_or_permissions(manage_channels=True)
+    async def setreee(self, ctx):
+        server = ctx.message.server
+        if server.id not in self.settings:
+            self.settings.append(server.id)
+            await self.bot.say("REEE images will now be posted in {}!".format(server.name))
+        elif server.id in self.settings:
+            self.settings.remove(server.id)
+            await self.bot.say("REEE images will no longer be posted in {}!".format(server.name))
+        dataIO.save_json("data/reee/settings.json", self.settings)
+
+def check_folder():
+    if not os.path.exists("data/reee"):
+        print("Creating data/reee folder")
+        os.makedirs("data/reee")
+
+def check_file():
+    data = []
+    f = "data/reee/settings.json"
+    if not dataIO.is_valid_json(f):
+        print("Creating default settings.json...")
+        dataIO.save_json(f, data)
 
 def setup(bot):
+    check_folder()
+    check_file()
     if not importavailable:
         raise NameError("You need to run `pip3 install pillow` and `pip3 install numpy`")
     n = Reee(bot)
