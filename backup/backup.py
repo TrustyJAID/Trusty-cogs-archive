85b139267a339579a966befe109c6295553736bf
TrustyJAID <TrustyJAID@gmail.com>
2018-01-16 20:34:17 -0700

    [Backup] Added saving of images to a folder and dedicated command
    Former-commit-id: 6cd59700174d24a3a39121640007b13532f4a242 [formerly d551bdbafb15e38aa750cb65a5481d332dfcc470]
    Former-commit-id: 9685823398ad941023be82033968f374b11dd3a8

--- backup/backup.py
+++ backup/backup.py
@@ -1,5 +1,6 @@
 import discord
 import asyncio
+import aiohttp
 from discord.ext import commands
 from .utils.dataIO import dataIO
 from cogs.utils import checks
@@ -11,21 +12,58 @@ class Backup:
 
     def __init__(self, bot):
         self.bot = bot
+        self.session = aiohttp.ClientSession(loop=self.bot.loop)
 
 
     async def check_folder(self, folder_name):
-        if not os.path.exists("data/backup/{}".format(folder_name)):
+        if not os.path.exists("data/backup/{}".format(folder_name)) or not os.path.exists("data/backup/{}/files".format(folder_name)):
             try:
                 os.makedirs("data/backup/{}".format(folder_name))
+                os.makedirs("data/backup/{}/files".format(folder_name))
                 return True
             except:
                 return False
         else:
             return True
+
+    @commands.command(pass_context=True, aliases=["dlimage"])
+    @checks.admin_or_permissions(manage_messages=True)
+    async def imagedl(self, ctx, *, server_name=None):
+        if server_name is None:
+            server = ctx.message.server
+        else:
+            for servers in self.bot.servers:
+                if server_name.lower() in server.name:
+                    server = servers
+        channel = ctx.message.channel
+        is_folder = await self.check_folder(server.name)
+        total_msgs = 0
+        if not is_folder:
+            print("{} folder doesn't exist!".format(server.name))
+            return
+        for chn in server.channels:
+            # await self.bot.send_message(channel, "backing up {}".format(chn.name))
+            files_saved = 0
+            try:
+                async for message in self.bot.logs_from(chn, limit=10000000):
+                    if message.attachments != []:
+                        for file in message.attachments:
+                            async with self.session.get(file["url"]) as resp:
+                                data = await resp.read()
+                            file_loc = "data/backup/{}/files/{}".format(server.name, file["filename"])
+                            with open(file_loc, "wb") as file:
+                                file.write(data)
+                        files_saved += 1
+                    total_msgs += 1
+                await self.bot.send_message(channel, "{} messages saved from {}".format(files_saved, chn.name))
+            except discord.errors.Forbidden:
+                await self.bot.send_message(channel, "0 messages saved from {}".format(chn.name))
+                pass
+        await self.bot.send_message(channel, "{} messages saved from {}".format(total_msgs, server.name))
             
 
 
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, aliases=["backup"])
     @checks.admin_or_permissions(manage_messages=True)
     async def logs(self, ctx, *, server_name=None):
         if server_name is None:
@@ -70,6 +108,13 @@ class Backup:
                             "id":message.id,
                             "attachments":message.attachments,
                             "pinned":message.pinned}
+                    if message.attachments != []:
+                        for file in message.attachments:
+                            async with self.session.get(file["url"]) as resp:
+                                img_data = await resp.read()
+                            file_loc = "data/backup/{}/files/{}".format(server.name, file["filename"])
+                            with open(file_loc, "wb") as file:
+                                file.write(img_data)
                     message_list.append(data)
                 total_msgs += len(message_list)
                 if len(message_list) == 0:
