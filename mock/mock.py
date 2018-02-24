79555be2e020ab16c112a4e3a3895b6c112185a8
TrustyJAID <TrustyJAID@gmail.com>
2018-02-24 00:49:51 -0700

    [Mock] Updated to V3

---
+++ mock/mock.py
@@ -0,0 +1,59 @@
+import discord
+import random
+from discord.ext import commands
+
+class Mock:
+    """mock a user as spongebob"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    def cap_change(self, message):
+        result = ""
+        for char in message:
+            value = random.choice([True, False])
+            if value:
+                result += char.upper()
+            else:
+                result += char.lower()
+        return result
+
+    @commands.command(pass_context=True)
+    async def mock(self, ctx, *, msg=""):
+        """Use >mock to randomized capitalization on a message or string.
+            Better random comming soon™
+            Usage:
+            >mock a string
+                A sTRiNg
+            >mock
+                laSt SeNT MeSsaGE
+            >mock (message id)
+                THaT mEsSAgE
+        """
+        channel = ctx.message.channel
+        result = ""
+        user = ctx.message.author
+        if msg:
+            if msg.isdigit():
+                async for message in channel.history(limit=100):
+                    if str(message.id) == msg:
+                        msg = message
+                        break
+        else:
+            async for message in channel.history(limit=2):
+                msg = message
+        result = self.cap_change(msg.content) if hasattr(msg, "content") else self.cap_change(msg)
+        author = msg.author if hasattr(msg, "author") else ctx.message.author
+        time = msg.created_at if hasattr(msg, "created_at") else ctx.message.created_at
+        # file = discord.File("data/mock/spongebob.jpg")
+        embed = discord.Embed(description=result,
+                              timestamp=time)
+        embed.colour = author.colour if hasattr(author, "colour") else discord.Colour.default()
+        embed.set_author(name=author.display_name, icon_url=author.avatar_url)
+        embed.set_thumbnail(url="https://i.imgur.com/upItEiG.jpg")
+        embed.set_footer(text="{} mocked {}".format(ctx.message.author.display_name, author.display_name), icon_url=ctx.message.author.avatar_url)
+        if hasattr(msg, "attachments") and msg.attachments != []:
+            embed.set_image(url=msg.attachments[0].url)
+        await ctx.send(embed=embed)
+        if author != user:
+            await ctx.send("- " + author.mention)
