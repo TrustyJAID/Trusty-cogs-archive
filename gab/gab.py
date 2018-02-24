f3faf777fb0dca8b212b749d39d6eb09420f2ebb
TrustyJAID <TrustyJAID@gmail.com>
2018-02-24 01:30:15 -0700

    [Gab] Updated to V3
    Former-commit-id: bfc0e91479c12070259e4dde098dab27c8ec7c46 [formerly 044ebcaad2114ac372189af34b0e850b394b71cd]
    Former-commit-id: abe06b993fbd714e8efaeacabac125fbe78194d8

--- gab/gab.py
+++ gab/gab.py
@@ -1,7 +1,5 @@
 import discord
 from discord.ext import commands
-from .utils.chat_formatting import *
-from .utils.dataIO import dataIO
 from redbot.core import checks
 from redbot.core import Config
 import asyncio
@@ -17,83 +15,27 @@ class Gab:
         self.bot = bot
         # self.tags = dataIO.load_json("data/gab/gabtags.json")
         self.config = Config.get_conf(self, 17864784635)
-        default_user = {"gab_tag":""}
+        default_user = {"gab_tag":None}
         default_guild = {"channel":None, "role_add":None, "role_remove":None, "require_gab":False}
-        default_global = {"login":{"username":"", "password":""}}
+        default_global = {"login":{"username":"", "password":""}, "cookies":{}}
         self.config.register_user(**default_user)
         self.config.register_guild(**default_guild)
         self.config.register_global(**default_global)
-        self.guilds = ["261565811309674499", "321105104931389440"]
         self.api_link = "https://gab.ai/auth/login"
-        # self.settings = dataIO.load_json("data/gab/settings.json")
         self.search_user = "https://gab.ai/users/{}"
 
     @commands.group(pass_context=True)
     async def gab(self, ctx, usertag=""):
         """Add your gab tag to receive the role Anonymous"""
         if ctx.invoked_subcommand is None:
-            await ctx.invoke(self.register_gab)    
-
-    @gab.command(hidden=True)
-    async def patreon(self):
-        """Help on petreon!"""
-        msg = "Support <@142525247357321216>  and their guild/work here: https://www.patreon.com/gabanon"
-        await ctx.send(msg)
-
-    @gab.command(hidden=True, pass_context=True)
-    async def invite(self, ctx):
-        """Invite link to gabanon"""
-        if ctx.message.guild.id in self.guilds:
-            await ctx.send("https://discord.gg/WswbEcJ")
-
-    @gab.command(pass_context=True)
-    @checks.admin_or_permissions(manage_roles=True)
-    async def change(self, ctx, usertag, username: discord.Member=None):
-        """Lets a user change their gab tag"""
-        guild = ctx.message.guild.id
-        if guild not in self.guilds:
-            await ctx.send("This only works on the GabAnon guild!")
-            return
-
-            if username is None:
-                username = str(ctx.message.author)
-            if username in self.tags[guild]:
-                await ctx.send("You have not supplied a gab tag before, please \
-                                   type `;gab gabtag` to be added to the list!")
-            if usertag not in self.tags[guild]:
-                self.save_tags(guild, usertag, username)
-                if username == "":
-                    msg = "{0} Your gab tag has been updated to {1}!"
-                    await ctx.send(msg.format(ctx.message.author.mention,
-                                                  usertag))
-                else:
-                    msg = "@{0} Your gab tag has been updated to {1}!"
-                    await ctx.send(msg.format(username, usertag))
-
-    @gab.command(pass_context=True)
-    @checks.admin_or_permissions(manage_roles=True)
-    async def remove(self, ctx, gabtag):
-        """Remomves gab tag and user"""
-        guild = ctx.message.guild.id
-        tags = self.tags[guild]
-        if gabtag in tags.values():
-            for key, value in tags.items():
-                if gabtag in value:
-                    del tags[key]
-                    break
-            await ctx.send("{} has been removed from the list!".format(gabtag))
-            dataIO.save_json("data/gab/gabtags.json", self.tags)
-        else:
-            msg = "That gab tag is not in the list or has already been removed!"
-            await ctx.send(msg)
+            await ctx.invoke(self.register_gab, usertag)
 
 
     def get_roles(self, ctx, role):
-        return {r.name: r for r in ctx.message.guild.roles}[role]
+        return {r.id: r for r in ctx.message.guild.roles}[role]
 
     async def addgabrole(self, ctx, role):
-        await asyncio.sleep(2)
-        await self.bot.add_roles(ctx.message.author, self.get_roles(ctx, role))
+        await ctx.message.author.add_roles(self.get_roles(ctx, role))
         return
 
     async def check_gab_usernames(self, username):
@@ -103,13 +45,12 @@ class Gab:
                 data = await resp.text()
                 soup = BeautifulSoup(data, "html.parser")
                 token = soup.find("input", {"name": "_token"}).get("value")
-                login = {"username":self.settings["login"]["username"], 
-                         "password":self.settings["login"]["password"], 
+                login = {"username":await self.config.login.username(), 
+                         "password":await self.config.login.password(), 
                          "_token":token, 
                          "remember": True}
             async with session.post(self.api_link, data=login) as resp:
-                self.settings["cookies"] = resp.cookies
-                dataIO.save_json("data/gab/settings.json", self.settings)
+                await self.config.cookies.set(resp.cookies)
             async with session.get(self.search_user.format(username)) as resp:
                 data = await resp.json()
                 if "status" in data:
@@ -118,25 +59,39 @@ class Gab:
                     return True
     
     @gab.command(pass_context=True)
-    async def set(self, ctx, channel: discord.Channel, role_add: discord.Role, role_remove: discord.Role=None):
+    async def set(self, ctx, channel: discord.TextChannel, role_add: discord.Role, role_remove: discord.Role=None):
         guild = ctx.message.guild
-        if guild.id not in self.tags:
-            self.tags[guild.id] = {}
-        self.save_tags(guild.id, role_add.name, "role_add")
-        self.save_tags(guild.id, channel.id, "channel")
-        if role_remove is not None:
-            self.save_tags(guild.id, role_remove.name, "role_remove")
+        await self.config.guild(guild).channel.set(channel.id)
+        await self.config.guild(guild).role_add.set(role_add.id)
+        await self.config.guild(guild).role_remove.set(role_remove)
+        await self.config.guild(guild).require_gab.set(True)
         await ctx.send("Accepting gab tags in {} and applying role {}".format(channel, role_add))
+
+    @gab.command()
+    async def toggle(self, ctx):
+        guild = ctx.message.guild
+        if await self.config.guild(guild).require_gab():
+            await self.config.guild(guild).require_gab.set(False)
+            await ctx.send("Accepting gab tags now.")
+        else:
+            await self.config.guild(guild).require_gab.set(True)
+            await ctx.send("Not accepting gab tags now.")
+
+    @gab.command()
+    async def login(self, ctx, username, password):
+        await self.config.login.username.set(username)
+        await self.config.login.password.set(password)
+        await ctx.send("Login info set for gab.")
 
     @gab.command(pass_context=True)
     async def register_gab(self, ctx, usertag:str):
         guild = ctx.message.guild
         guildname = ctx.message.guild.name
-        channel = ctx.message.channel.id
-        if not await self.config.guild(guild).require_gab:
+        channel = ctx.message.channel
+        if not await self.config.guild(guild).require_gab():
             return
-        correct_channel = self.bot.get_channel(id=await self.config.guild(guild).channel)
-        if channel != correct_channel.id:
+        correct_channel = guild.get_channel(await self.config.guild(guild).channel())
+        if channel != correct_channel:
             await ctx.send("Please supply the gab tag in {}!".format(correct_channel.mention))
             return
 
@@ -150,26 +105,9 @@ class Gab:
             await ctx.send("That gab account does not exist! Please try again or ask for some help.")
             return
 
-        if await self.config.user(author) != "":
-            await ctx.send("You have already supplied a gab tag {}!"
-                               .format(ctx.message.author.mention))
-            return
-
-        else:
-            self.save_tags(guild, usertag, username)
-            await self.addgabrole(ctx, self.tags[guild]["role_add"])
-            if guild == "261565811309674499":
-                # await self.addgabrole(ctx, "Anonymous")
-                await self.addgabrole(ctx, "Guest")
-            await ctx.send("Hello {0}, welcome to {1}!"
-                               .format(ctx.message.author.mention, guildname))
-            if "role_remove" in self.tags[guild]:
-                await self.bot.role_remove(ctx.message.author, self.get_roles(ctx, self.tags[guild]["role_remove"]))
-
-    async def on_member_join(self, member):
-        guild = member.guild
-        if await self.config.guild(guild).require_gab:
-            if await self.config.user(author) != "":
-                await self.addgabrole(ctx, await self.config.guild(guild).add_role)
-                if await self.config.guild(guild).role_remove != None:
-                    await member.role_removes(self.get_roles(await self.config.guild(guild).role_remove))
+        await self.config.user(author).gab_tag.set(usertag)
+        await self.addgabrole(ctx, await self.config.guild(guild).role_add())
+        await ctx.send("Hello {0}, welcome to {1}!"
+                           .format(ctx.message.author.mention, guildname))
+        if await self.config.guild(guild).role_remove() is not None:
+            await ctx.message.author.role_remove(self.get_roles(ctx, await self.config.guild(guild).role_remove()))
