ecc893848f7d1224e00ebd72a5cb5fbca3354f4a
TrustyJAID <TrustyJAID@gmail.com>
2018-02-25 20:50:54 -0700

    [Translate] Updated to V3

--- translate/translate.py
+++ translate/translate.py
@@ -1,8 +1,10 @@
 import aiohttp
 import discord
 from discord.ext import commands
-from .utils.dataIO import dataIO
-from .utils import checks
+from redbot.core import Config
+from redbot.core import checks
+from redbot.core.data_manager import bundled_data_path
+import json
 from .flags import flags
 
 '''Translator cog 
@@ -24,32 +26,26 @@ class Translate:
         self.bot = bot
         self.session = aiohttp.ClientSession(loop=self.bot.loop)
         self.url = "https://translation.googleapis.com"
-        self.settings = dataIO.load_json("data/translate/settings.json")
-        self.languages = flags
-
-    @commands.command(pass_context=True)
-    @checks.is_owner()
-    async def addflag(self, ctx, flag_1, flag_2=None):
-        """Sets a custom flag to another flag"""
-        new_data = []
-        if flag_2 is not None:
-            new_data = self.languages[flag_2]
-        if flag_1 not in self.languages:
-            self.languages[flag_1] = new_data
-            dataIO.save_json("data/google/flags.json", self.languages)
-        else:
-            await self.bot.say("{} is already in the list!".format(flag_1))
+        self.config = Config.get_conf(self, 156434873547)
+        default_guild = {"enabled":False}
+        default = {"api_key":None}
+        self.config.register_guild(**default_guild)
+        self.config.register_global(**default)
+        # with open(str(cog_data_path(self) + "/flags.json"), "r") as file:
+            # self.flags = json.loads(file.read())
+        self.flags = flags
+        # self.settings = dataIO.load_json("data/translate/settings.json")
 
     @commands.command(pass_context=True)
     async def translate(self, ctx, to_language, *, message):
-        if self.settings["key"] is None:
+        if await self.config.api_key() is None:
             await self.bot.say("The bot owner needs to set an api key first!")
             return
-        if to_language in self.languages:
-            language_code = self.languages[to_language]["code"]
+        if to_language in self.flags:
+            language_code = self.flags[to_language]["code"]
         else:
             try:
-                language_code = [self.languages[lang]["code"] for lang in self.languages if (self.languages[lang]["name"].lower() in to_language.lower()) or (self.languages[lang]["country"].lower() in to_language.lower())][0]
+                language_code = [self.flags[lang]["code"] for lang in self.flags if (self.flags[lang]["name"].lower() in to_language.lower()) or (self.flags[lang]["country"].lower() in to_language.lower())][0]
             except IndexError:
                 await self.bot.say("{} is not an available language!".format(to_language))
                 return
@@ -60,17 +56,17 @@ class Translate:
         em = discord.Embed(colour=author.top_role.colour, description=translated_text)
         em.set_author(name=author.display_name, icon_url=author.avatar_url)
         em.set_footer(text="{} to {}".format(from_lang[0][0]["language"].upper(), language_code.upper()))
-        await self.bot.send_message(ctx.message.channel, embed=em)
+        await ctx.send(embed=em)
 
     async def detect_language(self, text):
-        async with self.session.get(self.url + "/language/translate/v2/detect", params={"q":text, "key":self.settings["key"]}) as resp:
+        async with self.session.get(self.url + "/language/translate/v2/detect", params={"q":text, "key":await self.config.api_key()}) as resp:
                 data = await resp.json()
         return data["data"]["detections"]
 
 
     async def translate_text(self, from_lang, target, text):
         formatting = "text"
-        params = {"q":text, "target":target,"key":self.settings["key"], "format":formatting, "source":from_lang}
+        params = {"q":text, "target":target,"key":await self.config.api_key(), "format":formatting, "source":from_lang}
         try:
             async with self.session.get(self.url + "/language/translate/v2", params=params) as resp:
                 data = await resp.json()
@@ -83,39 +79,39 @@ class Translate:
     @commands.command(pass_context=True)
     @checks.mod_or_permissions(manage_channels=True)
     async def translatereact(self, ctx):
-        """Set the bot to post reaction flags to translate messages on this guild"""
+        """Set the bot to post reaction self.flags to translate messages on this guild"""
         guild = ctx.message.guild
-        if "guilds" not in self.settings:
-            self.settings["guilds"] = []
-        if guild.id not in self.settings["guilds"]:
-            self.settings["guilds"].append(guild.id)
-            await self.bot.say("{} has been added to post translated responses!".format(guild.name))
-        elif guild.id in self.settings["guilds"]:
-            self.settings["guilds"].remove(guild.id)
-            await self.bot.say("{} has been removed from translated responses!".format(guild.name))
-        dataIO.save_json("data/google/settings.json", self.settings)
+        if not await self.config.guild(guild).enabled():
+            await self.config.guild(guild).enabled.set(True)
+            await ctx.send("{} has been added to post translated responses!".format(guild.name))
+        elif await self.config.guild(guild).enabled():
+            await self.config.guild(guild).enabled.set(False)
+            await ctx.send("{} has been removed from translated responses!".format(guild.name))
 
-
-    async def on_reaction_add(self, reaction, user):
+    async def on_raw_reaction_add(self,emoji, message_id, channel_id, user_id):
         """Translates the message based off the flag added"""
-        if self.settings["key"] is None:
+        channel = self.bot.get_channel(id=channel_id)
+        message = await channel.get_message(id=message_id)
+        guild = channel.guild
+        if await self.config.api_key() is None:
             return
-        if reaction.emoji not in self.languages:
+        # check_emoji = lambda emoji: emoji in self.flags
+        if str(emoji) not in self.flags:
             return
-        if reaction.message.channel.guild.id not in self.settings["guilds"]:
+        if not await self.config.guild(guild).enabled():
             return
-        if reaction.message.embeds != []:
-            to_translate = reaction.message.embeds[0]["description"]
+        if message.embeds != []:
+            to_translate = message.embeds[0]["description"]
         else:
-            to_translate = reaction.message.clean_content    
-        target = self.languages[reaction.emoji]["code"]
+            to_translate = message.clean_content    
+        target = self.flags[str(emoji)]["code"]
         from_lang = await self.detect_language(to_translate)
         translated_text = await self.translate_text(from_lang[0][0]["language"], target, to_translate)
-        author = reaction.message.author
+        author = message.author
         em = discord.Embed(colour=author.top_role.colour, description=translated_text)
         em.set_author(name=author.display_name, icon_url=author.avatar_url)
         em.set_footer(text="{} to {}".format(from_lang[0][0]["language"].upper(), target.upper()))
-        await self.bot.send_message(reaction.message.channel, embed=em)
+        await channel.send(embed=em)
 
     @commands.command(pass_context=True)
     @checks.is_owner()
@@ -123,23 +119,5 @@ class Translate:
         """You must get an API key from google to set this up
         https://console.cloud.google.com/apis/library/translate.googleapis.com/?q=translate&id=c22f20ba-6a29-40ae-9084-8bc264a97fc2&project=dameznet-158219
         """
-        self.settings["key"] = api_key
-        dataIO.save_json("data/google/settings.json", self.settings)
-        await self.bot.say("API key set.")
-
-def check_folder():
-    if not os.path.exists("data/translate"):
-        print("Creating data/translate folder")
-        os.makedirs("data/translate")
-
-def check_file():
-    data = {"key": None, "guilds": []}
-    f = "data/translate/settings.json"
-    if not dataIO.is_valid_json(f):
-        print("Creating default settings.json...")
-        dataIO.save_json(f, data)
-
-def setup(bot):
-    check_folder()
-    check_file()
-    bot.add_cog(Translate(bot))
+        await self.config.api_key.set(api_key)
+        await ctx.send("API key set.")
