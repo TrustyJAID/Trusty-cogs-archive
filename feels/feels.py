ac04de05777ef1d977f6de7d1bef14be2642c8f7
TrustyJAID <TrustyJAID@gmail.com>
2018-03-20 13:26:10 -0600

    [Feels] Added info.json and improved gif support
    Former-commit-id: b9311e98fed1f498624e57c3484bfba2e72245ad [formerly f2df84d300a5c82ee126a76b38c2b8ecb719f531]
    Former-commit-id: c8fadcbd9cd50555cfc4caae62888af2ca8cd446

--- feels/feels.py
+++ feels/feels.py
@@ -44,7 +44,7 @@ class Feels:
             # ext = await self.make_feels(user)
             await ctx.send(file=file)
 
-    def make_animated_gif(self, user, avatar):
+    def make_animated_gif(self, colour, avatar):
         gif_list = [frame.copy() for frame in ImageSequence.Iterator(avatar)]
         img_list = []
         num = 0
@@ -52,33 +52,31 @@ class Feels:
         for frame in gif_list:
             template = Image.open(str(bundled_data_path(self)) + "/pepetemplate.png")
             template = template.convert("RGBA")
-            colour = user.top_role.colour.to_rgb()
             transparency = template.split()[-1].getdata()
             data = np.array(template)
             red, green, blue, alpha = data.T
-            blue_areas = (red == 0) & (blue > 205) & (green == 0) & (alpha > 50)
+            blue_areas = (red == 0) & (blue == 255) & (green == 0) & (alpha == 255)
             data[..., :-1][blue_areas.T] = colour
             temp2 = Image.fromarray(data)
             frame = frame.convert("RGBA")
             frame = frame.rotate(-30, expand=True)
-            frame = frame.resize((150, 150), Image.ANTIALIAS)
-            temp2.paste(frame, (150, 60), frame)
+            frame = frame.resize((60, 60), Image.ANTIALIAS)
+            temp2.paste(frame, (40, 25), frame)
             # temp2.thumbnail((320, 320), Image.ANTIALIAS)
             img_list.append(temp2)
             num += 1
             temp = BytesIO()
             temp2.save(temp, format="GIF", save_all=True, append_images=img_list, duration=0, loop=0, transparency=0)
-            temp.name = "{}.gif".format(user.display_name)
+            temp.name = "feels.gif"
             if sys.getsizeof(temp) < 8000000 and sys.getsizeof(temp) > 7000000:
                 break
         return temp
 
-    def make_feels_img(self, user, avatar):
+    def make_feels_img(self, colour, avatar):
         template = Image.open(str(bundled_data_path(self)) + "/pepetemplate.png")
         # print(template.info)
         template = template.convert("RGBA")
-        colour = user.top_role.colour.to_rgb()
-        print(colour)
+        
         # avatar = Image.open(self.files + "temp." + ext)
         transparency = template.split()[-1].getdata()
         data = np.array(template)
@@ -89,27 +87,35 @@ class Feels:
         temp2 = temp2.convert("RGBA")
         avatar = avatar.convert("RGBA")
         avatar = avatar.rotate(-30, expand=True)
-        avatar = avatar.resize((150, 150), Image.ANTIALIAS)
-        temp2.paste(avatar, (150, 60), avatar)
+        avatar = avatar.resize((60, 60), Image.ANTIALIAS)
+        temp2.paste(avatar, (40, 25), avatar)
         temp = BytesIO()
         temp2.save(temp, format="PNG")
-        temp.name = "{}.png".format(user.display_name)
+        temp.name = "feels.png"
         return temp
 
 
     async def make_feels(self, user):
 
         if user.is_avatar_animated():
-            avatar = Image.open(await self.dl_image(user.avatar_url_as(format="gif")))
-            task = functools.partial(self.make_animated_gif, user=user, avatar=avatar)
+            avatar = Image.open(await self.dl_image(user.avatar_url_as(format="gif", size=64)))
+            try:
+                colour = user.top_role.colour.to_rgb()
+            except:
+                colour = (255, 255, 255)
+            task = functools.partial(self.make_animated_gif, colour=colour, avatar=avatar)
             task = self.bot.loop.run_in_executor(None, task)
             try:
                 temp = await asyncio.wait_for(task, timeout=60)
             except asyncio.TimeoutError:
                 return
         else:
-            avatar = Image.open(await self.dl_image(user.avatar_url_as(format="png")))
-            task = functools.partial(self.make_feels_img, user=user, avatar=avatar)
+            avatar = Image.open(await self.dl_image(user.avatar_url_as(format="png", size=64)))
+            try:
+                colour = user.top_role.colour.to_rgb()
+            except:
+                colour = (0, 0, 0)
+            task = functools.partial(self.make_feels_img, colour=colour, avatar=avatar)
             task = self.bot.loop.run_in_executor(None, task)
             try:
                 temp = await asyncio.wait_for(task, timeout=60)
