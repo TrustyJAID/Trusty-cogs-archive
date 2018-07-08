391ecf4d3f56c0c9e065c330493bea76caec9f0d
TrustyJAID <TrustyJAID@gmail.com>
2018-07-08 00:17:34 -0600

    [ServerWhitelist] V2 version
    Former-commit-id: edb7b9d87e2b115fabfb93f2beeb39bccdec5ffb [formerly 804bc71b223d832358e3036f2379e3a8ac40b2db]
    Former-commit-id: 680f6359122a20be3474284b71c6f095e8b41ac5

---
+++ serverwhitelist/serverwhitelist.py
@@ -0,0 +1,63 @@
+import discord
+from discord.ext import commands
+from .utils.chat_formatting import box
+from .utils.dataIO import dataIO
+from .utils import checks
+
+class ServerWhitelist:
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.settings_file = "data/serverwhitelist/settings.json"
+        self.settings = dataIO.load_json(self.settings_file)
+
+    @commands.group(name="serverwhitelist", aliases=["serverwhitelist"])
+    async def serverwhitelist(self, ctx):
+        """Add or remove servers to the bots approved server list"""
+        if ctx.invoked_subcommand is None:
+            await self.bot.send_cmd_help(ctx)
+
+    @serverwhitelist.command(aliases=["set"])
+    async def add(self, ctx, server_id:int):
+        """Add a server to the bots approved server list."""
+        if server_id in self.settings["whitelist"]:
+            await self.bot.send_message(ctx.message.channel, "{} is already in the whitelist.".format(server_id))
+            return
+        self.settings["whitelist"].append(server_id)
+        dataIO.save_json(self.settings_file, self.settings)
+        await self.bot.send_message(ctx.message.channel, "{} server has been added to the whitelist.".format(server_id))
+
+
+    @serverwhitelist.command()
+    async def remove(self, ctx, server_id:int):
+        """Removes a server from the bots approved server list."""
+        if server_id not in self.settings["whitelist"]:
+            await self.bot.send_message(ctx.message.channel, "{} is not in the whitelist.".format(server_id))
+            return
+        self.settings["whitelist"].remove(server_id)
+        dataIO.save_json(self.settings_file, self.settings)
+        await self.bot.send_message(ctx.message.channel, "{} server has been removed the whitelist.".format(server_id))
+
+    async def on_server_join(self, server):
+        """Checks if a joined server is in the bots approved server list and leaves if it isn't."""
+        if server.id not in self.settings["whitelist"]:
+            await self.bot.leave_server(server)
+
+def check_folder():
+    if not os.path.exists("data/serverwhitelist"):
+        print("Creating data/serverwhitelist folder")
+        os.makedirs("data/serverwhitelist")
+
+
+def check_file():
+    data = {"whitelist":[]}
+    f = "data/serverwhitelist/settings.json"
+    if not dataIO.is_valid_json(f):
+        print("Creating default settings.json...")
+        dataIO.save_json(f, data)
+
+def setup(bot):
+    n = ServerWhitelist(bot)
+    bot.add_cog(n)
+
+
