2eac6870b418cc3f0702910634c9ec1e7907d27a
TrustyJAID <TrustyJAID@gmail.com>
2018-07-13 11:27:14 -0600

    [Insult] Rewritten for V3 and moved Compliments to new cog

--- insult/insults.py
+++ insult/insults.py
@@ -1,4 +1,4 @@
-[
+insults = [
     "Yo Mama so fat she sued Xbox 360 for guessing her weight.",
     "You're so fat that when you were diagnosed with a flesh eating bacteria - the doctors gave you 87 years to live.",
     "You're so fat you've got more chins than a Hong Kong phone book.",
