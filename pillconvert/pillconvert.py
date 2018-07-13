7bc6df0aa30d5edf383eb6795fb6fb477467ca30
TrustyJAID <TrustyJAID@gmail.com>
2018-07-13 12:09:29 -0600

    [Pillconvert] Make image manipulation async
    Former-commit-id: 27104a8362adb3b272957993b24446c5997d3854 [formerly 41617f67cd8f2db14a26c3e68b65e538656bd4a5]
    Former-commit-id: e56f986483cce8e32636bf8591c34f9a5afad8c9

--- pillconvert/pillconvert.py
+++ pillconvert/pillconvert.py
@@ -2,67 +2,21 @@ import discord
 from discord.ext import commands
 from redbot.core import checks
 from redbot.core.data_manager import bundled_data_path
-from redbot.core.data_manager import cog_data_path
 from PIL import Image
 from PIL import ImageColor
 import numpy as np
+import asyncio
+import functools
+from io import BytesIO
 
 
 class PillConvert:
 
     def __init__(self, bot):
         self.bot = bot
-        self.path = cog_data_path(self)
-        self.bundle_path = bundled_data_path(self)
 
-    @commands.command()
-    async def printpath(self, ctx):
-        print(self.path)
-
-    @commands.command(aliases=["rp"])
-    async def redpill(self, ctx):
-        """Red Pill"""
-        await self.colorconvert("#FF0000")
-        image = discord.File(str(self.path) + "\\pill.png")
-        await ctx.send(file=image)
-
-    @commands.command(aliases=["bp"])
-    async def bluepill(self, ctx):
-        """Blue Pill"""
-        await self.colorconvert("#0000FF")
-        image = discord.File(str(self.path) + "\\pill.png")
-        await ctx.send(file=image)
-
-    @commands.command(aliases=["blp"])
-    async def blackpill(self, ctx):
-        """Black Pill"""
-        await self.colorconvert("#008000")
-        image = discord.File(str(self.path) + "\\pill.png")
-        await ctx.send(file=image)
-
-    @commands.command(aliases=["pp"])
-    async def purplepill(self, ctx):
-        """Purple Pill"""
-        await self.colorconvert("#800080")
-        image = discord.File(str(self.path) + "\\pill.png")
-        await ctx.send(file=image)
-
-    @commands.command(aliases=["yp"])
-    async def yellowpill(self, ctx):
-        """Yellow Pill"""
-        await self.colorconvert("#FFFF00")
-        image = discord.File(str(self.path) + "\\pill.png")
-        await ctx.send(file=image)
-
-    @commands.command(aliases=["gp"])
-    async def greenpill(self, ctx):
-        """Green Pill"""
-        await self.colorconvert("#008000")
-        image = discord.File(str(self.path) + "\\pill.png")
-        await ctx.send(file=image)
-
-    async def colorconvert(self, colour="#FF0000"):
-        im = Image.open(str(self.bundle_path) + "\\blackpill.png")
+    def colour_convert(self, colour="#FF0000"):
+        im = Image.open(str(bundled_data_path(self)) + "/blackpill.png")
         im = im.convert('RGBA')
         colour = ImageColor.getrgb(colour)
         data = np.array(im)
@@ -70,12 +24,88 @@ class PillConvert:
         white_areas = (red == 0) & (blue == 0) & (green == 0) & (alpha == 255)
         data[..., :-1][white_areas.T] = colour
         im2 = Image.fromarray(data)
-        im2.save(str(self.path) + "\\pill.png")
+        temp = BytesIO()
+        im2.save(temp, format="PNG")
+        temp.name = "pill.png"
+        return temp
+
+    @commands.command(aliases=["rp"])
+    async def redpill(self, ctx):
+        """Red Pill"""
+        pill_image = await self.make_colour("#FF0000")
+        if pill_image is None:
+                await ctx.send("Something went wrong sorry!")
+                return
+        image = discord.File(pill_image)
+        await ctx.send(file=image)
+
+    @commands.command(aliases=["bp"])
+    async def bluepill(self, ctx):
+        """Blue Pill"""
+        pill_image = await self.make_colour("#0000FF")
+        if pill_image is None:
+                await ctx.send("Something went wrong sorry!")
+                return
+        image = discord.File(pill_image)
+        await ctx.send(file=image)
+
+    @commands.command(aliases=["blp"])
+    async def blackpill(self, ctx):
+        """Black Pill"""
+        pill_image = await self.make_colour("#008000")
+        if pill_image is None:
+                await ctx.send("Something went wrong sorry!")
+                return
+        image = discord.File(pill_image)
+        await ctx.send(file=image)
+
+    @commands.command(aliases=["pp"])
+    async def purplepill(self, ctx):
+        """Purple Pill"""
+        pill_image = await self.make_colour("#800080")
+        if pill_image is None:
+                await ctx.send("Something went wrong sorry!")
+                return
+        image = discord.File(pill_image)
+        await ctx.send(file=image)
+
+    @commands.command(aliases=["yp"])
+    async def yellowpill(self, ctx):
+        """Yellow Pill"""
+        pill_image = await self.make_colour("#FFFF00")
+        if pill_image is None:
+                await ctx.send("Something went wrong sorry!")
+                return
+        image = discord.File(pill_image)
+        await ctx.send(file=image)
+
+    @commands.command(aliases=["gp"])
+    async def greenpill(self, ctx):
+        """Green Pill"""
+        pill_image = await self.make_colour("#008000")
+        if pill_image is None:
+                await ctx.send("Something went wrong sorry!")
+                return
+        image = discord.File(pill_image)
+        await ctx.send(file=image)
+
+    async def make_colour(self, colour):
+        task = functools.partial(self.colour_convert,colour=colour)
+        task = self.bot.loop.run_in_executor(None, task)
+        try:
+            image = await asyncio.wait_for(task, timeout=60)
+        except asyncio.TimeoutError:
+            return
+        image.seek(0)
+        return image
 
     @commands.command()
     async def pill(self, ctx, colour="#FF0000"):
         """Converts the pill to any colour with hex codes like #FF0000"""
         await ctx.trigger_typing()
-        await self.colorconvert(colour)
-        image = discord.File(str(self.path) + "\\pill.png")
+        pill_image = await self.make_colour(colour)
+        if pill_image is None:
+                await ctx.send("Something went wrong sorry!")
+                return
+        image = discord.File(pill_image)
         await ctx.send(file=image)
