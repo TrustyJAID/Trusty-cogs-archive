524146904fb2fe3e3e8d712a61a1763daadad600
TrustyJAID <TrustyJAID@gmail.com>
2018-09-26 23:12:13 -0600

    [Imagemaker] Merged pillconvert and trump cogs into image maker cog
    Former-commit-id: 15a23a0b44b6731ea8b501812a53bc002c815335 [formerly caac2c838a479ab032c99aef17fe2c3a7809aaaa]
    Former-commit-id: 9b8efe69086fece4511c2a03596162c435571b81

--- pillconvert/__init__.py
+++
@@ -1,8 +0,0 @@
-from .pillconvert import PillConvert
-from redbot.core import data_manager
-
-
-def setup(bot):
-    n = PillConvert(bot)
-    data_manager.load_bundled_data(n, __file__)
-    bot.add_cog(n)
