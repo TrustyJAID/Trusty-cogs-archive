f41e848f9eb4ae516ae73975e2db954f199fc75e
TrustyJAID <TrustyJAID@gmail.com>
2018-09-30 12:08:30 -0600

    [Cleverbot] Added cleverbot.io API access as an option cleaned up unnecessary code
    Former-commit-id: 622456d1233a8a4442ac7b3a4adf551a24f629eb [formerly 6e8865aa26a0a9ad626010a40cc2a081d719705a]
    Former-commit-id: 57dd3a3a814323fb9342cdd0e4dbb9f521dd3de0

--- cleverbot/__init__.py
+++ cleverbot/__init__.py
@@ -1,7 +1,5 @@
 from .cleverbot import Cleverbot
-from redbot.core import data_manager
 
 def setup(bot):
     cog = Cleverbot(bot)
-    data_manager.load_bundled_data(cog, __file__)
     bot.add_cog(cog)
