e6b4842fb2695c624f2e279a59d1b04eafb63dfd
TrustyJAID <TrustyJAID@gmail.com>
2018-09-30 20:56:25 -0600

    [NotSoBot] Actually fix vw imports

--- notsobot/vw/macintoshplus.py
+++ notsobot/vw/macintoshplus.py
@@ -33,11 +33,11 @@ japanese_corpus = '''それは20年前の今日だった
 そして、サージェント·ペパーズ·ロンリー·ハーツ·クラブ·バンド'''.split('\n')
 print(str(__file__)[:38])
 main_dir = str(__file__)[:38]
-bubbles = [main_dir+'/img/png/bubbles/' + i for i in os.listdir(main_dir+'/img/png/bubbles/') if i != 'Thumbs.db']
-windows = [main_dir+'/img/png/windows/' + i for i in os.listdir(main_dir+'/img/png/windows/') if i != 'Thumbs.db']
-backgrounds = [main_dir+'/img/png/background/' + i for i in os.listdir(main_dir+'/img/png/background/') if i != 'Thumbs.db']
-pics = [main_dir+'/img/png/pics/' + i for i in os.listdir(main_dir+'/img/png/pics/') if i != 'Thumbs.db']
-greek = [main_dir+'/img/png/greek/' + i for i in os.listdir(main_dir+'/img/png/greek/') if i != 'Thumbs.db']
+bubbles = [main_dir+'/cogs/notsobot/vw/img/png/bubbles/' + i for i in os.listdir(main_dir+'/cogs/notsobot/vw/img/png/bubbles/') if i != 'Thumbs.db']
+windows = [main_dir+'/cogs/notsobot/vw/img/png/windows/' + i for i in os.listdir(main_dir+'/cogs/notsobot/vw/img/png/windows/') if i != 'Thumbs.db']
+backgrounds = [main_dir+'/cogs/notsobot/vw/img/png/background/' + i for i in os.listdir(main_dir+'/cogs/notsobot/vw/img/png/background/') if i != 'Thumbs.db']
+pics = [main_dir+'/cogs/notsobot/vw/img/png/pics/' + i for i in os.listdir(main_dir+'/cogs/notsobot/vw/img/png/pics/') if i != 'Thumbs.db']
+greek = [main_dir+'/cogs/notsobot/vw/img/png/greek/' + i for i in os.listdir(main_dir+'/cogs/notsobot/vw/img/png/greek/') if i != 'Thumbs.db']
 
 def random_color(k=0):
 	RGB = int(k%255),int(255*cos(k)),int(255*(1-sin(k)))
