e461b4d71b54ad55b834be39664c21e5250ee5d1
TrustyJAID <TrustyJAID@gmail.com>
2018-10-03 15:51:26 -0600

    [Dev] Update to current core cog
    Former-commit-id: 9984d6a9c96f1176668a91492b22f97a57e35bd0 [formerly eca5dc08d50fe31eb3590b472a6c905e7f0eda9e]
    Former-commit-id: f86e7a09c16296f647eeca385cb5590a7ba750a6

--- dev/dev.py
+++ dev/dev.py
@@ -3,27 +3,33 @@ import inspect
 import io
 import textwrap
 import traceback
+import re
 from contextlib import redirect_stdout
 from copy import copy
 
 import discord
-from redbot.core import commands
-from redbot.core import checks
-from redbot.core.i18n import Translator
-from redbot.core.utils.chat_formatting import box, pagify
+from . import checks, commands
+from .i18n import Translator
+from .utils.chat_formatting import box, pagify
+
 """
 Notice:
+
 95% of the below code came from R.Danny which can be found here:
+
 https://github.com/Rapptz/RoboDanny/blob/master/cogs/repl.py
 """
 
 _ = Translator("Dev", __file__)
 
+START_CODE_BLOCK_RE = re.compile(r"^((```py)(?=\s)|(```))")
+
+
 class Dev(getattr(commands, "Cog", object)):
     """Various development focused utilities."""
 
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self):
+        super().__init__()
         self._last_result = None
         self.sessions = set()
 
@@ -31,23 +37,21 @@ class Dev(getattr(commands, "Cog", object)):
     def cleanup_code(content):
         """Automatically removes code blocks from the code."""
         # remove ```py\n```
-        if content.startswith('```') and content.endswith('```'):
-            return '\n'.join(content.split('\n')[1:-1])
+        if content.startswith("```") and content.endswith("```"):
+            return START_CODE_BLOCK_RE.sub("", content)[:-3]
 
         # remove `foo`
-        return content.strip('` \n')
+        return content.strip("` \n")
 
     @staticmethod
     def get_syntax_error(e):
         """Format a syntax error to send to the user.
+
         Returns a string representation of the error formatted as a codeblock.
         """
         if e.text is None:
-            return box('{0.__class__.__name__}: {0}'.format(e), lang="py")
-        return box(
-            '{0.text}{1:>{0.offset}}\n{2}: {0}'
-            ''.format(e, '^', type(e).__name__),
-            lang="py")
+            return box("{0.__class__.__name__}: {0}".format(e), lang="py")
+        return box("{0.text}{1:>{0.offset}}\n{2}: {0}".format(e, "^", type(e).__name__), lang="py")
 
     @staticmethod
     def get_pages(msg: str):
@@ -68,12 +72,15 @@ class Dev(getattr(commands, "Cog", object)):
     @checks.is_owner()
     async def debug(self, ctx, *, code):
         """Evaluate a statement of python code.
+
         The bot will always respond with the return value of the code.
         If the return value of the code is a coroutine, it will be awaited,
         and the result of that will be the bot's response.
+
         Note: Only one statement may be evaluated. Using await, yield or
         similar restricted keywords will result in a syntax error. For multiple
         lines or asynchronous code, see [p]repl or [p]eval.
+
         Environment Variables:
             ctx      - command invokation context
             bot      - bot object
@@ -81,19 +88,19 @@ class Dev(getattr(commands, "Cog", object)):
             author   - command author's member object
             message  - the command's message object
             discord  - discord.py library
-            commands - discord.py commands extension
+            commands - redbot.core.commands
             _        - The result of the last dev command.
         """
         env = {
-            'bot': ctx.bot,
-            'ctx': ctx,
-            'channel': ctx.channel,
-            'author': ctx.author,
-            'guild': ctx.guild,
-            'message': ctx.message,
-            'discord': discord,
-            'commands': commands,
-            '_': self._last_result
+            "bot": ctx.bot,
+            "ctx": ctx,
+            "channel": ctx.channel,
+            "author": ctx.author,
+            "guild": ctx.guild,
+            "message": ctx.message,
+            "discord": discord,
+            "commands": commands,
+            "_": self._last_result,
         }
 
         code = self.cleanup_code(code)
@@ -104,8 +111,7 @@ class Dev(getattr(commands, "Cog", object)):
             await ctx.send(self.get_syntax_error(e))
             return
         except Exception as e:
-            await ctx.send(
-                box('{}: {!s}'.format(type(e).__name__, e), lang='py'))
+            await ctx.send(box("{}: {!s}".format(type(e).__name__, e), lang="py"))
             return
 
         if asyncio.iscoroutine(result):
@@ -117,15 +123,18 @@ class Dev(getattr(commands, "Cog", object)):
 
         await ctx.send_interactive(self.get_pages(result), box_lang="py")
 
-    @commands.command(name='eval')
+    @commands.command(name="eval")
     @checks.is_owner()
     async def _eval(self, ctx, *, body: str):
         """Execute asynchronous code.
+
         This command wraps code into the body of an async function and then
         calls and awaits it. The bot will respond with anything printed to
         stdout, as well as the return value of the function.
+
         The code can be within a codeblock, inline code or neither, as long
         as they are not mixed and they are formatted correctly.
+
         Environment Variables:
             ctx      - command invokation context
             bot      - bot object
@@ -133,32 +142,32 @@ class Dev(getattr(commands, "Cog", object)):
             author   - command author's member object
             message  - the command's message object
             discord  - discord.py library
-            commands - discord.py commands extension
+            commands - redbot.core.commands
             _        - The result of the last dev command.
         """
         env = {
-            'bot': ctx.bot,
-            'ctx': ctx,
-            'channel': ctx.channel,
-            'author': ctx.author,
-            'guild': ctx.guild,
-            'message': ctx.message,
-            'discord': discord,
-            'commands': commands,
-            '_': self._last_result
+            "bot": ctx.bot,
+            "ctx": ctx,
+            "channel": ctx.channel,
+            "author": ctx.author,
+            "guild": ctx.guild,
+            "message": ctx.message,
+            "discord": discord,
+            "commands": commands,
+            "_": self._last_result,
         }
 
         body = self.cleanup_code(body)
         stdout = io.StringIO()
 
-        to_compile = 'async def func():\n%s' % textwrap.indent(body, '  ')
+        to_compile = "async def func():\n%s" % textwrap.indent(body, "  ")
 
         try:
             exec(to_compile, env)
         except SyntaxError as e:
             return await ctx.send(self.get_syntax_error(e))
 
-        func = env['func']
+        func = env["func"]
         result = None
         try:
             with redirect_stdout(stdout):
@@ -182,50 +191,52 @@ class Dev(getattr(commands, "Cog", object)):
     @checks.is_owner()
     async def repl(self, ctx):
         """Open an interactive REPL.
+
         The REPL will only recognise code as messages which start with a
         backtick. This includes codeblocks, and as such multiple lines can be
         evaluated.
+
         You may not await any code in this REPL unless you define it inside an
         async function.
         """
         variables = {
-            'ctx': ctx,
-            'bot': ctx.bot,
-            'message': ctx.message,
-            'guild': ctx.guild,
-            'channel': ctx.channel,
-            'author': ctx.author,
-            '_': None,
+            "ctx": ctx,
+            "bot": ctx.bot,
+            "message": ctx.message,
+            "guild": ctx.guild,
+            "channel": ctx.channel,
+            "author": ctx.author,
+            "_": None,
         }
 
         if ctx.channel.id in self.sessions:
-            await ctx.send(_('Already running a REPL session in this channel. '
-                             'Exit it with `quit`.'))
+            await ctx.send(
+                _("Already running a REPL session in this channel. Exit it with `quit`.")
+            )
             return
 
         self.sessions.add(ctx.channel.id)
-        await ctx.send(_('Enter code to execute or evaluate.'
-                         ' `exit()` or `quit` to exit.'))
+        await ctx.send(_("Enter code to execute or evaluate. `exit()` or `quit` to exit."))
 
-        msg_check = lambda m: (m.author == ctx.author and
-                               m.channel == ctx.channel and
-                               m.content.startswith('`'))
+        msg_check = lambda m: (
+            m.author == ctx.author and m.channel == ctx.channel and m.content.startswith("`")
+        )
 
         while True:
             response = await ctx.bot.wait_for("message", check=msg_check)
 
             cleaned = self.cleanup_code(response.content)
 
-            if cleaned in ('quit', 'exit', 'exit()'):
-                await ctx.send('Exiting.')
+            if cleaned in ("quit", "exit", "exit()"):
+                await ctx.send("Exiting.")
                 self.sessions.remove(ctx.channel.id)
                 return
 
             executor = exec
-            if cleaned.count('\n') == 0:
+            if cleaned.count("\n") == 0:
                 # single statement, potentially 'eval'
                 try:
-                    code = compile(cleaned, '<repl session>', 'eval')
+                    code = compile(cleaned, "<repl session>", "eval")
                 except SyntaxError:
                     pass
                 else:
@@ -233,12 +244,12 @@ class Dev(getattr(commands, "Cog", object)):
 
             if executor is exec:
                 try:
-                    code = compile(cleaned, '<repl session>', 'exec')
+                    code = compile(cleaned, "<repl session>", "exec")
                 except SyntaxError as e:
                     await ctx.send(self.get_syntax_error(e))
                     continue
 
-            variables['message'] = response
+            variables["message"] = response
 
             stdout = io.StringIO()
 
@@ -256,7 +267,7 @@ class Dev(getattr(commands, "Cog", object)):
                 value = stdout.getvalue()
                 if result is not None:
                     msg = "{}{}".format(value, result)
-                    variables['_'] = result
+                    variables["_"] = result
                 elif value:
                     msg = "{}".format(value)
 
@@ -267,24 +278,26 @@ class Dev(getattr(commands, "Cog", object)):
             except discord.Forbidden:
                 pass
             except discord.HTTPException as e:
-                await ctx.send(_('Unexpected error: `{}`').format(e))
+                await ctx.send(_("Unexpected error: `{}`").format(e))
 
     @commands.command()
     @checks.is_owner()
     async def sudo(self, ctx, user: discord.Member, *, command):
         """Mock another user invoking a command.
+
         The prefix must not be entered.
         """
         msg = copy(ctx.message)
         msg.author = user
         msg.content = ctx.prefix + command
 
-        ctx.bot.dispatch('message', msg)
+        ctx.bot.dispatch("message", msg)
 
-    @commands.command(name="mockmsg")
+    @commands.command(name="sudomsg")
     @checks.is_owner()
     async def mock_msg(self, ctx, user: discord.Member, *, content: str):
         """Dispatch a message event as if it were sent by a different user.
+
         Only reads the raw content of the message. Attachments, embeds etc. are
         ignored.
         """
