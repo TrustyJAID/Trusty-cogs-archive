682eb5718b2a5d39d0bf8075c3575d70aaa24276
TrustyJAID <TrustyJAID@gmail.com>
2018-10-22 07:55:37 -0600

    [Encoding] Change name of encode/decode caeser cipher function

--- encoding/encoding.py
+++ encoding/encoding.py
@@ -114,18 +114,18 @@ class Encoding(getattr(commands, "Cog", object)):
         return lambda s: s.translate(lookup)
 
     @_encode.command(name="rot", aliases=["caeser"])
-    async def caeser_cipher(self, ctx, key:int, *, message:str):
+    async def caeser_encode(self, ctx: commands.Context, rot_key:int, *, message:str):
         """
             Encode a caeser cipher message with specified key
         """
-        await ctx.send(self.rot_encode(key)(message))
+        await ctx.send(self.rot_encode(rot_key)(message))
 
     @_decode.command(name="rot", aliases=["caeser"])
-    async def caeser_cipher(self, ctx, key:int, *, message:str):
+    async def caeser_decode(self, ctx: commands.Context, rot_key:int, *, message:str):
         """
             Decode a caeser cipher message with specified key
         """
-        await ctx.send(self.rot_encode(-key)(message))
+        await ctx.send(self.rot_encode(-rot_key)(message))
 
 
     @_encode.command(name="dna")
