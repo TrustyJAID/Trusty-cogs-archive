8718794cdf7d9cad492b83c2a34d2bdda8308ee7
TrustyJAID <TrustyJAID@gmail.com>
2018-11-25 17:51:38 -0700

    Remove special cogs from V3 branch

--- trustyavatar/__init__.py
+++
@@ -1,7 +0,0 @@
-from .trustyavatar import TrustyAvatar
-from redbot.core import data_manager
-
-def setup(bot):
-    n = TrustyAvatar(bot)
-    data_manager.load_bundled_data(n, __file__)
-    bot.add_cog(n)
