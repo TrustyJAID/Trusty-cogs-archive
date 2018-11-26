0e3a833ba42121b4ae645434a11bb9c1ecf912e2
TrustyJAID <TrustyJAID@gmail.com>
2018-11-25 17:50:55 -0700

    Leave only special cogs in special cogs branch
    Former-commit-id: ffa02a3c3ed4933c3e3b5c8dce70d866eb204416 [formerly 1f710c7e98f98a6adbb02fa9db267c5dff86974b]
    Former-commit-id: 3c89a843c20bf366915b66ef66b409a99edcda15

--- triggersize/__init__.py
+++
@@ -1,8 +0,0 @@
-from .triggersize import Triggersize
-from redbot.core import data_manager
-
-def setup(bot):
-    n = Triggersize(bot)
-    data_manager.load_bundled_data(n, __file__)
-    bot.add_cog(n)
-
